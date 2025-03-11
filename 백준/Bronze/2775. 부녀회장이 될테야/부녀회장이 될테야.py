8bf44645e9c93d3ba6a0ffa453409a984e665701
github-lily <jhc03032@naver.com>
2025-03-11 19:35:55 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 96 ms, Memory: 109544 KB -BaekjoonHub

---
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
@@ -0,0 +1,15 @@
+T = int(input())
+for _ in range(T) :
+    k = int(input())
+    n = int(input())
+
+    dp = [0] * n
+    for w in range(1,n+1) :
+        dp[w-1] = w
+
+    for _ in range(1, k+1) :
+        for i in range(1,n) :
+            dp[i] += dp[i-1]
+
+    
+    print(dp[n-1])
