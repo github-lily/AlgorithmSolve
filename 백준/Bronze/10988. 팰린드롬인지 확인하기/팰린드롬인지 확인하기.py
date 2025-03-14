4b1805788a26f87362e90e3e75aff885e8140bd1
github-lily <jhc03032@naver.com>
2025-03-14 22:28:17 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.py
@@ -0,0 +1,21 @@
+def check() :
+    global word, N, half
+    if N % 2 == 0 :
+        for i in range(half) :
+            if word[half-i] != word[half+i-1] :
+                return 0
+
+    else :
+        for i in range(1,half+1) :
+            if word[half-i] != word[half+i] :
+                return 0
+            
+    return 1
+
+
+word = input()
+N = len(word)
+half = N // 2
+
+ans = check()
+print(ans)
