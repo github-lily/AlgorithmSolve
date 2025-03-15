2f4fb2f3f27860ab6679c20c12ad53c8bea1f1b2
github-lily <jhc03032@naver.com>
2025-03-15 11:24:49 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 100 ms, Memory: 109544 KB -BaekjoonHub

---
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.py
@@ -0,0 +1,12 @@
+def find() :
+    for x in range(-999,1000) :
+        for y in range(-999,1000) : 
+            if a*x + b*y == c and d*x + e*y ==f :
+                return x,y
+
+a,b,c,d,e,f = map(int,input().split())
+
+
+x,y = find()
+
+print(x,y)
