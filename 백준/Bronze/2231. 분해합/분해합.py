966ac2d956c6da5e788e1b25c22e7e64d7a1f5e8
github-lily <jhc03032@naver.com>
2025-03-16 18:15:24 +0900

    [Bronze II] Title: 분해합, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.py
+++ 백준/Bronze/2231. 분해합/분해합.py
@@ -1,24 +1,19 @@
-def check() :
-    for ans in range(n,N) :
-        ans_str = str(ans)
-        lenth = len(ans_str)
-        num = ans
-        # 자리수 더하기
-        for i in range(lenth) :
-            num += int(ans_str[i])
-        
-        # 일치하면 중단
-        if num == N :
-            return ans
-    
-    return 0
-        
-
-
 N = int(input())
-M = len(str(N))     # 최대값
-n = max(1, int(N) - 9*M)    # 최소값
 
+# 가능한 생성자의 최소 범위 설정
+start = max(0, N - 9 * len(str(N)))
 
-ans = check()
-print(ans)
+# 브루트포스 탐색
+for num in range(start, N):
+    temp, digit_sum = num, 0
+
+    # 각 자리수 합 계산
+    while temp:
+        digit_sum += temp % 10
+        temp //= 10
+
+    if num + digit_sum == N:
+        print(num)
+        break
+else:
+    print(0)
