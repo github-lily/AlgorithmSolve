0936fd8f3e431f0337d67d1e3059554bb355b605
github-lily <jhc03032@naver.com>
2025-05-05 15:50:01 +0900

    [Silver IV] Title: 큐, Time: 148 ms, Memory: 112904 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.py
@@ -0,0 +1,36 @@
+import sys
+input = sys.stdin.readline
+
+from collections import deque
+
+N = int(input())
+q = deque()
+
+for _ in range(N) :
+    cmd = list(input().strip().split())
+    req = cmd[0]
+    # print(req)
+    if req == 'push' :
+        q.append(cmd[1])
+    
+    elif req == 'size' :
+        print(len(q))
+    
+    else :
+        if q :
+            if req == 'pop' :
+                print(q.popleft())
+
+            elif req == 'empty' :
+                print(0)
+            elif req == 'front' :
+                print(q[0])
+            
+            elif req == 'back' :
+                print(q[-1])
+        else :
+            if req == 'empty' :
+                print(1)
+            else :
+                print(-1)
+            
