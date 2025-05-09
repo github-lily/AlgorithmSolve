279a71a0fe1b36762edad3b1bc72c6ef26c0af1d
github-lily <jhc03032@naver.com>
2025-05-09 21:48:40 +0900

    [Bronze III] Title: 핸드폰 요금, Time: 84 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/1267. 핸드폰 요금/핸드폰 요금.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+times = map(int,input().split())
+
+Y = 0
+M = 0
+
+for time in times :
+    Y += ((time // 30) + 1) * 10
+    M += ((time // 60) + 1) * 15
+
+if Y < M :
+    print("Y", Y)
+elif Y > M :
+    print("M", M)
+else :
+    print("Y M", Y)
