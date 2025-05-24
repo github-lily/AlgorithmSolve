120d252b3d819181f032595606224ca7b7e6617f
github-lily <jhc03032@naver.com>
2025-05-24 18:30:52 +0900

    [Bronze III] Title: 집 주소, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/1284. 집 주소/집 주소.py
+++ 백준/Bronze/1284. 집 주소/집 주소.py
@@ -1,14 +1,16 @@
 while True:
-    n = input()
-    if n == '0':
+    num = input()
+    if num == '0':
         break
+
     width = 1  # 맨 앞 여백
-    for digit in n:
+    for digit in num:
         if digit == '1':
             width += 2
         elif digit == '0':
             width += 4
         else:
             width += 3
-        width += 1  # 각 숫자 뒤 여백 추가
+        width += 1  # 숫자 사이 여백
+
     print(width)
