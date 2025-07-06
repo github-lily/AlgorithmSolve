1fd6a92fb84ea3619c987c028043f2ed1987f6c5
github-lily <jhc03032@naver.com>
2025-07-06 15:43:27 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 372 ms, Memory: 244752 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -0,0 +1,14 @@
+from collections import deque
+
+N,K = map(int,input().split())
+q = deque(range(1,N+1))
+result = []
+
+while q :
+    for _ in range(K-1) :
+        q.append(q.popleft())
+    result.append(q.popleft())
+
+ans = '<{}>'.format(", ".join(map(str,result)))
+
+print(ans)
