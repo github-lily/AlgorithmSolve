add8b4bea9c6303ca5b31700c9946da06afb79ee
github-lily <jhc03032@naver.com>
2025-07-09 23:36:57 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 128 ms, Memory: 110736 KB -BaekjoonHub

---
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
@@ -0,0 +1,12 @@
+T = int(input())
+
+for _ in range(T):
+    change = int(input())
+    q = change // 25
+    change %= 25
+    d = change // 10
+    change %= 10
+    n = change // 5
+    change %= 5
+    p = change
+    print(q, d, n, p)
