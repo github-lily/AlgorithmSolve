8696b2acc9845cc2f88c070f8ca193c361836067
github-lily <jhc03032@naver.com>
2025-07-26 23:56:42 +0900

    [Gold IV] Title: 플로이드, Time: 136 ms, Memory: 110928 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.py
+++ 백준/Gold/11404. 플로이드/플로이드.py
@@ -16,18 +16,14 @@ for k in range(1,N+1) :
 for _ in range(M) :
     s,e,v = map(int,input().split())
     # 노선이 여러개이므로 작은 값으로 저장
-    if graph[s][e] >= v :
+    if graph[s][e] > v :
         graph[s][e] = v
 
 for k in range(1,N+1) :
     for i in range(1,N+1) :
         for j in range(1,N+1) :
+            # i -> j와 i -> k -> j 를 비교해서 더 짧은 가중치 선택
             graph[i][j] = min(graph[i][j],graph[i][k]+graph[k][j])
 
 for i in range(1,N+1) :
-    for j in range(1,N+1) :
-        if graph[i][j] == INF :
-            print("0", end=" ")
-        else :
-            print(graph[i][j], end=" ")
-    print()
+    print(' '.join(['0' if graph[i][j] == INF else str(graph[i][j]) for j in range(1,N+1)]))
