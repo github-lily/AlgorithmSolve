641789830c67815eb2ec568a187e181e849aef46
github-lily <jhc03032@naver.com>
2025-08-06 00:42:24 +0900

    [Bronze I] Title: 달팽이2, Time: 96 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Bronze/1952. 달팽이2/달팽이2.py
+++ 백준/Bronze/1952. 달팽이2/달팽이2.py
@@ -1,28 +1,29 @@
-M, N = map(int, input().split())
+import sys
+input = sys.stdin.readline
 
-# 표 생성 및 방문 체크
-visited = [[False] * N for _ in range(M)]
-# 방향: → ↓ ← ↑
-dx = [0, 1, 0, -1]
-dy = [1, 0, -1, 0]
 
-x, y, d = 0, 0, 0  # 시작 좌표와 방향
-visited[x][y] = True
-turns = 0
+M,N = map(int,input().split())
+arr = [[0]*N for _ in range(M)]
 
-for _ in range(M * N - 1):  # 시작 칸은 이미 방문했으므로 -1
-    nx = x + dx[d]
-    ny = y + dy[d]
 
-    if 0 <= nx < M and 0 <= ny < N and not visited[nx][ny]:
-        x, y = nx, ny
-        visited[x][y] = True
-    else:
-        # 방향 전환
-        d = (d + 1) % 4
-        turns += 1
-        x += dx[d]
-        y += dy[d]
-        visited[x][y] = True
 
-print(turns)
+di,dj = [0,1,0,-1],[1,0,-1,0]  # 우, 하, 좌, 상
+i,j,d = 0,0,0             # 시작점, 방향
+arr[0][0] = 1           # 방문 체크
+cnt = 0                 # 꺾이는 횟수 카운트(끝에서 하나 추가됨)
+
+for _ in range(M*N-1) : # 이미 방문한 시작칸 제외 모두 돌 수 있도록
+    ni,nj = i+di[d], j+dj[d]
+    if 0<= ni < M and 0<= nj < N and arr[ni][nj] != 1 : # 범위이내이고 방문한 적 없다면
+        arr[ni][nj] = 1     # 방문 체크
+        i,j = ni,nj
+    
+    else :
+        d = (d+1) % 4
+        cnt += 1
+        i += di[d]
+        j += dj[d]
+        arr[i][j] = 1
+
+    
+print(cnt)
