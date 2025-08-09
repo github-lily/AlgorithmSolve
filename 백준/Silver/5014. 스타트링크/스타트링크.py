f76658e2387e9c38d6a73ab4dcdf0247d7a00559
github-lily <jhc03032@naver.com>
2025-08-09 22:46:35 +0900

    [Silver I] Title: 스타트링크, Time: 100 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Silver/5014. 스타트링크/스타트링크.py
+++ 백준/Silver/5014. 스타트링크/스타트링크.py
@@ -2,32 +2,26 @@
 import sys
 input = sys.stdin.readline
 
-from collections import deque
-
-def bfs(s, e) :
-    q = deque()
-    v = [0] * (F+1)
-
-    q.append(s)
-    v[s] = 1    # return 할 때는 -1 하기
-
-    while q :
-        c = q.popleft()
-
-        if c == e :
-            return v[c]-1
-        
-        # 2방향, 범위내, 미방문
-        for k in (c+U, c-D) :
-            if 1<=k<=F and v[k] == 0 :
-                q.append(k)
-                v[k] = v[c]+1
-
-    # 목적지를 찾을 수 없을 때
-    return "use the stairs"
-
 F,S,G,U,D = map(int,input().split())
+cnt = 0
 
-ans = bfs(S,G)
-print(ans)
-        
+while cnt <= 2000000 :
+    if S == G :
+        print(cnt)
+        exit()
+    
+    cnt += 1
+
+    # 현재 위치가 목표보다 높거나, S+U가 전체 층수보다 높거나, S-D가 1층 이상일 때
+    if (S>G or S+U > F) and S-D > 0 :
+        if D == 0 :
+            break
+        S -= D      # D층만큼 아래로
+
+    else :
+        S += U      # 위 경우가 아니면 위로
+    
+    if S > F :
+        break       # 만약 현 위치가 전체 층수보다 높으면 멈춤
+
+print("use the stairs")
