3b98ecf263f2edc4df72d3dedaff81c5a72f9b27
github-lily <jhc03032@naver.com>
2025-08-13 23:01:45 +0900

    [Gold V] Title: 적록색약, Time: 128 ms, Memory: 114176 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.py
+++ 백준/Gold/10026. 적록색약/적록색약.py
@@ -3,79 +3,45 @@ input = sys.stdin.readline
 
 from collections import deque
 
-def normal(i,j) :
-    global color
+def bfs(N,arr) :
+    cnt = 0             # 영역의 개수
+    v = [[0]*N for _ in range(N)]
 
-    q = deque()
-    q.append((i,j))
-    v[i][j] = 1
+    for i in range(N) :
+        for j in range(N) :
+            if v[i][j] == 0 :       # 미방문이라면 영역 탐색
+                color = arr[i][j] # 비교용 컬러 저장
+                cnt += 1            # 영역 개수 += 1
 
+                q = deque()
+                q.append((i,j))
+                v[i][j] = 1
 
+                while q :
+                    ci,cj = q.popleft()
 
-    while q :
-        ci,cj = q.popleft()
-
-        for di, dj in ((0,1),(1,0),(0,-1),(-1,0)) :
-            ni,nj = ci+di, cj+dj
-            # 범위내, 미방문, 같은 색
-            if 0<= ni < N and 0<= nj < N and v[ni][nj] == 0 and paint[ni][nj] == color :
-                v[ni][nj] = 1
-                q.append((ni,nj))
-
-
-def color_blind(i,j) :
-    global blind_color
-
-    q = deque()
-    q.append((i,j))
-    blind_v[i][j] = 1
-
-
-    while q :
-        ci,cj = q.popleft()
-
-        for di, dj in ((0,1),(1,0),(0,-1),(-1,0)) :
-            ni,nj = ci+di, cj+dj
-            # 범위내, 미방문
-            if 0<= ni < N and 0<= nj < N and blind_v[ni][nj] == 0 :
-                # 같은 색
-                if paint[ni][nj] == blind_color :
-                    blind_v[ni][nj] = 1
-                    q.append((ni,nj))
-                
-                # 같은색이 아니어도 적록색일 경우
-                elif blind_color == "R" or blind_color == "G":
-                    if paint[ni][nj] == "G" or paint[ni][nj] == "R" :
-                        blind_v[ni][nj] = 1
-                        q.append((ni,nj))
+                    for di, dj in ((0,1),(1,0),(0,-1),(-1,0)) :
+                        ni,nj = ci+di, cj+dj
+                        # 범위내, 미방문, 같은 색
+                        if 0<= ni < N and 0<= nj < N and v[ni][nj] == 0 and arr[ni][nj] == color :
+                            v[ni][nj] = 1
+                            q.append((ni,nj))
+    
+    return cnt
 
 
 
 N = int(input())
-paint = [list(input().strip()) for _ in range(N)]   # 문자열 값받기 주의~~
+
+# 문자열 각각을 받는 방법
+# paint = [list(input().strip()) for _ in range(N)]  
+
+# 문자열을 리스트로 쪼개지 않고 그대로 받는 방법
+paint = [input().strip() for _ in range(N)]     # ['RRRBB', 'GGBBB', 'BBBRR', 'BBRRR', 'RRRRR']
 
 
-# 영역의 개수
-cnt = blind_cnt = 0
+normal_cnt = bfs(N,paint)
+blind_cnt = bfs(N, [i.replace('R','G') for i in paint])
 
-# 정상인과 색맹의 방문 배열
-v = [[0]*N for _ in range(N)]
-blind_v = [[0]*N for _ in range(N)]
-
-
-
-for i in range(N) :
-    for j in range(N) :
-        if v[i][j] == 0 :       # 미방문이라면 영역 탐색
-            color = paint[i][j] # 비교용 컬러 저장
-            cnt += 1            # 영역 개수 += 1
-            normal(i,j)
-        
-        if blind_v[i][j] == 0 :
-            blind_color = paint[i][j]
-            blind_cnt += 1
-            color_blind(i,j)
-
-
-
-print(cnt, blind_cnt)
+    
+print(normal_cnt, blind_cnt)
