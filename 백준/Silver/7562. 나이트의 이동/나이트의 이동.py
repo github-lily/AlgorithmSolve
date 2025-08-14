d4fb04d0d310452b93be1b65487f810d06de91b1
github-lily <jhc03032@naver.com>
2025-08-14 18:57:21 +0900

    [Silver I] Title: 나이트의 이동, Time: 252 ms, Memory: 121360 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.py
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.py
@@ -6,30 +6,29 @@ from collections import deque
 T = int(input())
 for _ in range(T) :
     N = int(input())
-    chess = [[0]*N for _ in range(N)]
-    v = [[0]*N for _ in range(N)]
-
     si,sj = map(int,input().split())
     ti,tj = map(int,input().split())
 
+    if si == ti and sj == tj :
+        print(0)
+        continue
+    
+    v = [[-1]*N for _ in range(N)]
+
     q = deque([(si,sj)])
-    v[si][sj] = 1       #나중에 -1 해주기
+    v[si][sj] = 0       
 
     d = [(1,2),(2,1),(2,-1),(1,-2),(-1,-2),(-2,-1),(-2,1),(-1,2)]
     while q :
         ci,cj = q.popleft()
 
         if ci == ti and cj == tj :
-            print(v[ti][tj]-1)
+            print(v[ti][tj])
             break
 
         for di,dj in d :
             ni,nj = ci+di, cj+dj
-            # 범위내, 미방문 또는 가중치최소
-            if 0<= ni < N and 0<= nj < N and ( v[ni][nj] == 0 or v[ni][nj] > (v[ci][cj]+1)) :
+            # 범위내, 미방문(deque니까 미방문했다는게 최초경로라는 뜻)
+            if 0<= ni < N and 0<= nj < N and  v[ni][nj] == -1  :
                 v[ni][nj] = v[ci][cj] + 1
                 q.append((ni,nj))
-
-    
-        
-    
