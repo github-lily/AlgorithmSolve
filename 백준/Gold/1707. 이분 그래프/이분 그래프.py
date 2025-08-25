d77b8216eb0b4a72b0e615c743a0827588c1380b
github-lily <jhc03032@naver.com>
2025-08-25 19:52:58 +0900

    [Gold IV] Title: 이분 그래프, Time: 616 ms, Memory: 188688 KB -BaekjoonHub

--- 백준/Gold/1707. 이분 그래프/이분 그래프.py
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.py
@@ -3,42 +3,33 @@ input = sys.stdin.readline
 
 from collections import deque
 
-def bfs(start) :
-    q = deque([start])
-    v[start] = 1
+def is_bipartite(V, adj):
+    v = [0] * (V + 1)  # 0: 미방문, 1 / -1: 두 색
 
-    while q :
-        c = q.popleft()
-        for w in graph[c] :
-            if v[w] == 0 :
-                if v[c] == 1 :
-                    v[w] = 2
-                else :
-                    v[w] = 1
-                q.append(w)
-            elif v[w] == v[c] :
-                return False
+    for s in range(1, V + 1):
+        if v[s] != 0:
+            continue
+        # s가 고립 정점이든 아니든, 새로운 컴포넌트 시작
+        v[s] = 1
+        q = deque([s])
+
+        while q:
+            cur = q.popleft()
+            for nxt in adj[cur]:
+                if v[nxt] == 0:
+                    v[nxt] = -v[cur]   # 한 줄로 색 반전
+                    q.append(nxt)
+                elif v[nxt] == v[cur]:
+                    return False               # 같은 색 이웃 → 이분그래프 아님
     return True
-    
 
+T = int(input())
+for _ in range(T):
+    V, E = map(int, input().split())
+    adj = [[] for _ in range(V + 1)]
+    for _ in range(E):
+        a, b = map(int, input().split())
+        adj[a].append(b)
+        adj[b].append(a)
 
-T = int(input())    
-for _ in range(T) :
-    V,E = map(int,input().split())
-    graph = [[] for _ in range(V+1)]
-    for _ in range(E) :
-        u,v = list(map(int,input().split()))
-        graph[v].append(u)
-        graph[u].append(v)
-
-
-    v = [0] * (V+1)
-
-    for i in range(1,V+1) :
-        if v[i] == 0 :
-            ans = bfs(i)
-            if not ans :
-                print("NO")
-                break
-    if ans :
-        print("YES")
+    print("YES" if is_bipartite(V, adj) else "NO")
