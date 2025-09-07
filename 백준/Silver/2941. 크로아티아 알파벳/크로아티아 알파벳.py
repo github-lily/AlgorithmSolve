0a726b7bb07f5d92991f6dd5d30191423262e7e4
github-lily <jhc03032@naver.com>
2025-09-07 15:20:43 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
@@ -1,29 +1,7 @@
 
 text = input()
-N = len(text)
-i = 0
-cnt = 0
-char = ["c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="]
-while i < N :
-    word1 = text[i:i+2]
-    word2 = text[i:i+3]
+text = text.replace("dz=","*")
+for p in ("c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z=") :
+    text = text.replace(p,"*")
 
-
-    if word2 in char :
-        cnt += 1
-        i += 3
-
-    
-    elif  word1 in char :
-        cnt += 1
-        i += 2
-
-
-
-    else :
-        cnt += 1
-        i += 1
-
-    
-print(cnt)
-        
+print(len(text))
