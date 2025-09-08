eb790ec0e19385d35b0577c8437c1a748c7b0c1d
github-lily <jhc03032@naver.com>
2025-09-08 12:20:15 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.py
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.py
@@ -1,18 +1,2 @@
 N = int(input())
-nums = 1
-
-while N > 0 :
-    nums *= N
-    N -= 1
-
-nums = list(str(nums))
-cnt = 0
-for i in range(len(nums)-1,-1,-1) :
-    if nums[i] == "0" :
-        cnt += 1
-    else :
-        break
-
-print(cnt)
-        
-            
+print(N//5 + N//25 + N//125)
