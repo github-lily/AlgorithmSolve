20b7838ca60cd54504d900448c073922ad80be74
github-lily <jhc03032@naver.com>
2025-09-19 02:34:01 +0900

    [level 3] Title: 네트워크, Time: 0.43 ms, Memory: 9.4 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,43 +1,23 @@
-from collections import deque
-
-
-
 def solution(n, computers):
-    answer = 0
-    graph = [[]  for _ in range(n)]
     v = [0] * n
+    cnt = 0
     
-
-    # 그래프 저장
-    for i in range(n) :
-        for j in range(i+1,n) :     # i == j 거르기
-            if computers[i][j] == 1 :
-                graph[i].append(j)
-                graph[j].append(i)      # 양방향      
-    
-    # 함수 구현부
-    def bfs(start) :
-        nonlocal v, graph
-
-        q = deque([start])
-        v[start] = 1
-
-        while q :
-            cur = q.popleft()
-            for next in graph[cur] :
-                if v[next] == 0 :
-                    v[next] = 1
-                    q.append(next)
-    
-    # 함수 호출부                    
-    for node in range(n) :
-        if v[node] == 0 :
-            bfs(node)
-            answer += 1
-            
-
+    def dfs(idx) :
+        nonlocal v
         
+        v[idx] = 1
+                
+        for j in range(n) :
+            if v[j] == 0 and computers[idx][j] == 1 :       # 방문안한 인접 정점
+                dfs(j)
+
     
     
+    for i in range(n) :
+        if v[i] == 0 :
+            dfs(i)
+            cnt += 1
+            
     
-    return answer
+    
+    return cnt
