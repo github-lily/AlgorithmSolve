ef8031015edbad4d862ed760ae7985970ff73c7b
github-lily <jhc03032@naver.com>
2025-09-19 21:36:25 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 88 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
@@ -1,27 +1,26 @@
 N = int(input())
-ans = 0
+cnt = 0
 for _ in range(N) :
     text = input()
-    n = len(text)
+    char = set()
+    
+    i = 0
+    flag = True
 
-    # 한 글자인 경우
-    if n == 1 :
-        ans += 1
+    while i < len(text) :
+        cur = text[i]
+        if text[i] in char :
+            flag = False
+            break
+        char.add(text[i])
+        i += 1
+        while i < len(text) :
+            next = text[i]
+            if cur != next :
+                break
+            i += 1
+    
+    if flag :
+        cnt += 1
 
- 
-    # 한 글자 이상인 경우
-    else :
-        t_set = set(text[0])
-        flag = True
-        for i in range(1, len(text)) :
-            if text[i] in t_set :               # 중복된 수가 나왔을 때
-                if text[i-1] != text[i] :       # 이전값과 다르다면 중단
-                    flag = False
-                    break
-            else :
-                t_set.add(text[i])
-        if flag == True :
-            ans += 1
-            
-
-print(ans)
+print(cnt)
