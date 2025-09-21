0eabd86cbe1923c96b0ad8405c72a63f8d87807d
github-lily <jhc03032@naver.com>
2025-09-22 00:04:21 +0900

    [Silver III] Title: 계단 오르기, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.py
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -1,15 +1,23 @@
 import sys
 input = sys.stdin.readline
 
-n = int(input())
-stairs = [0] + [int(input()) for _ in range(n)]
-dp = [0] * (n+1)
+N = int(input())
+stairs = [0]*(N+1)   # 시작점 0
+for idx in range(1,N+1) :
+    stairs[idx] = int(input())
 
-if n == 1:
+dp = [0] * (N+1)
+
+if N == 1 :
     print(stairs[1])
-else:
+
+else :
     dp[1] = stairs[1]
-    dp[2] = stairs[1] + stairs[2]
-    for i in range(3, n+1):
-        dp[i] = max(dp[i-2], dp[i-3] + stairs[i-1]) + stairs[i]
-    print(dp[n])
+    dp[2] = dp[1] + stairs[2]
+    for i in range(3,N+1) :
+        dp[i] = max(dp[i-2],dp[i-3] + stairs[i-1]) + stairs[i]
+        # 현재 계단에 오기 위한 조건을 고려한 최대값 계산
+        # 현재 계단을 밟으려면 2칸 전 계단을 밟았거나, 3칸전 계단과 이전 계단을 밟아야 함
+        # 이전 계단만 보면 연속일 수 있으므로 3칸전 계단도 함께 확인
+
+    print(dp[N])
