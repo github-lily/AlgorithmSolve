28da3e37d137c7444d4b42e75b53d52866197d53
이현희 <jhc03032@naver.com>
2025-11-03 23:08:15 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.22 ms, Memory: 9.16 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.py
@@ -0,0 +1,10 @@
+def solution(n):
+    MOD = 1234567
+    a, b = 1, 2   # n=1 -> 1, n=2 -> 2
+    if n == 1: 
+        return 1
+    if n == 2: 
+        return 2
+    for _ in range(3, n + 1):
+        a, b = b, (a + b) % MOD
+    return b % MOD
