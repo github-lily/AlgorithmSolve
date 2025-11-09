afddbdb4b989fe08f1618542b9a578d56933c5ea
이현희 <jhc03032@naver.com>
2025-11-09 23:27:56 +0900

    [Silver V] Title: 너의 평점은, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.py
@@ -0,0 +1,22 @@
+import sys
+
+grade = {
+    "A+": 4.5, "A0": 4.0,
+    "B+": 3.5, "B0": 3.0,
+    "C+": 2.5, "C0": 2.0,
+    "D+": 1.5, "D0": 1.0,
+    "F": 0.0,
+}
+
+total_score = 0.0
+total_credit = 0.0
+
+for _ in range(20):
+    name, credit, letter = sys.stdin.readline().split()
+    if letter == "P":
+        continue
+    c = float(credit)
+    total_credit += c
+    total_score += c * grade[letter]
+
+print(0.0 if total_credit == 0 else total_score / total_credit)
