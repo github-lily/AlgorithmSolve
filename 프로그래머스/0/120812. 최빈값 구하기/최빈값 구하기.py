7b03c0f92252c092325b102e48d866913ebe09d4
이현희 <jhc03032@naver.com>
2026-01-23 00:25:11 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.03 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.py
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.py
@@ -1,27 +1,29 @@
 from collections import defaultdict
 
 def solution(arr):
-    arr.sort()
     
     dic = defaultdict(int)
-    lst = []
     
     # 개수 카운트
     for a in arr :
         dic[a] += 1
     
-    mx = max(dic.values())
+    # 최빈값 cnt 찾기
+    mx_cnt = max(dic.values())
+    # 최빈값
+    mx = 0
+    cnt = 0     
     
-    # 최대값 원소 찾기
+    
+    # 최빈값 원소 찾기
     for k,v in dic.items() :
-        if v == mx :
-            lst.append(k)
-    
-    # 최빈값 반환
-    if len(lst) > 1 :
-        return -1
-    else :
-        return lst[0]
+        if v == mx_cnt :
+            mx = k
+            cnt += 1
+            if cnt > 1 :
+                return -1
+    return mx
+
     
     
 
