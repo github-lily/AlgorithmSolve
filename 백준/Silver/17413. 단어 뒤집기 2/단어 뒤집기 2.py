6a8dc4923248a933f3c98b0fdb24e7fd1ef9c8fe
이현희 <jhc03032@naver.com>
2026-01-31 18:14:05 +0900

    [Silver III] Title: 단어 뒤집기 2, Time: 108 ms, Memory: 118160 KB -BaekjoonHub

--- 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.py
+++ 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.py
@@ -1,38 +1,38 @@
+sentence = input()
 
-text = list(input())
+word = []
+ans = []
+is_tag = False
 
-s = 0
-e = 0
-n = len(text)
-ans = ''
-
-while e < n :
-    # 태그일 때 건너뛰기
-    if text[e] == '<' :     # 태그 시작
-        if s != e :         # 그동안 모아둔 단어가 있다면 뒤집기
-            ans += ''.join(text[s:e][::-1])
-            s = e           # 태그 시작으로 시작점 이동 
-
-        # 태그 끝 찾기
-        while e < n and text[e] != '>' :
-            e += 1
-        
-        # 태그 붙이기
-        ans += ''.join(text[s:e+1])
-        s = e +1
-
-    # 단어 뒤집기(공백은 단어 끝 의미)
-    elif text[e] == ' ' :
-        ans += ''.join(text[s:e][::-1])
-        ans += ' '
-        s = e + 1       # 시작 재설정
+for s in sentence :
+    # 태그
+    if s == '<' :
+        # 모아둔 단어 체크
+        if word :
+            ans.extend(reversed(word))
+            word.clear()
+        is_tag = True
+        ans.append(s)
     
-    # 문장 끝
-    elif e == n-1 :
-        ans += ''.join(text[s:e+1][::-1])
-    
-    e += 1
+    elif s == '>' :
+        is_tag = False
+        ans.append(s)
 
-print(ans)
+    elif is_tag :
+        ans.append(s)       # 태그 내부면 바로 넣기
 
+    # 단어
+    else :
+        if s == ' ' :
+            if word :
+                ans.extend(reversed(word))
+                word.clear()
+            ans.append(s)
+        else :
+            word.append(s)
 
+# 마지막 단어 넣기
+if word :
+    ans.extend(reversed(word))
+
+print(''.join(ans))
