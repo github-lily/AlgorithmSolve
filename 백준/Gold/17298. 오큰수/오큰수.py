55403a6b46ea601728a9421532a871547db1e2e8
이현희 <jhc03032@naver.com>
2026-02-01 14:03:13 +0900

    [Gold IV] Title: 오큰수, Time: 480 ms, Memory: 256456 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.py
+++ 백준/Gold/17298. 오큰수/오큰수.py
@@ -1,20 +1,13 @@
 N = int(input())
 A = list(map(int,input().split()))
 
-
 stack = []
 ans = [-1] * N
 
-for i in range(1, N) :
-    # 오큰수 저장
-    if A[i-1] < A[i] :
-        ans[i-1] = A[i]
-        while stack and A[stack[-1]] < A[i] :
-            si = stack.pop()    # stack index
-            ans[si] = A[i]    
-    # 오큰수 없으면 저장해두기
-    else :
-        stack.append(i-1)
+for i in range(N) :
+    while stack and A[stack[-1]] < A[i] :
+        si = stack.pop()    # stack index
+        ans[si] = A[i]    
+    stack.append(i)
 
 print(*ans)
-    
