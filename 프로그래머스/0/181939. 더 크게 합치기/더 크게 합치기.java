d4e0fca7946e1dd54056419ea2ba3b7539cf6ff5
이현희 <jhc03032@naver.com>
2026-02-11 01:55:28 +0900

    [level 0] Title: 더 크게 합치기, Time: 11.03 ms, Memory: 83.6 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
@@ -1,12 +1,5 @@
 class Solution {
     public int solution(int a, int b) {
-        int num1 = Integer.parseInt("" + a + b);
-        int num2 = Integer.parseInt("" + b + a);
-
-        if (num1 >= num2) {
-            return num1;
-        } else {
-            return num2;
-        }
+        return Math.max(Integer.parseInt(a+""+b), Integer.parseInt(b+""+a));
     }
 }
