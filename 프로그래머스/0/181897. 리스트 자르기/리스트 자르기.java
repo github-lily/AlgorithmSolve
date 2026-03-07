71bac7da342138a1c77666909e8c2bb0743ed1e4
이현희 <jhc03032@naver.com>
2026-03-07 16:48:45 +0900

    [level 0] Title: 리스트 자르기, Time: 0.01 ms, Memory: 81.4 MB -BaekjoonHub

--- 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.java
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.java
@@ -2,30 +2,15 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int n, int[] slicer, int[] num_list) {
-        ArrayList<Integer> ans = new ArrayList<>();
-
-        switch(n) {
-            case 1 -> {
-                for (int i = 0; i <= slicer[1]; i++) {
-                    ans.add(num_list[i]);
-                }
-            }
-            case 2 -> {
-                for (int i = slicer[0]; i < num_list.length; i++) {
-                    ans.add(num_list[i]);
-                }
-            }
-            case 3 -> {
-                for (int i = slicer[0]; i <= slicer[1]; i++) {
-                    ans.add(num_list[i]);
-                }
-            }
-            case 4 -> {
-                for (int i = slicer[0]; i <= slicer[1]; i += slicer[2]) {
-                    ans.add(num_list[i]);
-                }
-            }
+        int start = n == 1 ? 0 : slicer[0];
+        int end = n == 2 ? num_list.length - 1 : slicer[1];
+        int step = n == 4 ? slicer[2] : 1;
+        
+        int[] ans = new int[(end - start + step) / step];
+        for (int i = start, j = 0; i <= end; i += step) {
+            ans[j++] = num_list[i];
         }
-        return ans.stream().mapToInt(i->i).toArray();
+        
+        return ans;
     }
 }
