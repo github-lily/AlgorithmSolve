f4044e9c220059b511394c61a6b9a16733c0f9e9
이현희 <jhc03032@naver.com>
2026-03-09 21:59:39 +0900

    [level 0] Title: 배열 조각하기, Time: 23.49 ms, Memory: 83.1 MB -BaekjoonHub

--- 프로그래머스/0/181893. 배열 조각하기/README.md
+++ 프로그래머스/0/181893. 배열 조각하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 배열 조각하기 - 181893 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181893?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181893) 
 
 ### 성능 요약
 
-메모리: 94.4 MB, 시간: 0.86 ms
+메모리: 83.1 MB, 시간: 23.49 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 03월 08일 23:53:13
+2026년 03월 09일 21:59:40
 
 ### 문제 설명
 
--- 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.java
+++ 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.java
@@ -2,12 +2,25 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] arr, int[] query) {
-        for (int i = 0; i < query.length; i++) {
-            if (i % 2 == 0) { 
-                arr = Arrays.copyOfRange(arr, 0, query[i] + 1);
+        for (int idx = 0; idx < query.length; idx++) {
+            ArrayList<Integer> ans = new ArrayList<>();
+            
+            int s,e;
+            int q = query[idx];
+            
+            if (idx % 2 == 0) { 
+                s = 0;
+                e = q;
             } else {
-                arr = Arrays.copyOfRange(arr, query[i], arr.length);
+                s = q;
+                e = arr.length-1;
             }
+
+            for (int i = s; i<= e; i++) {
+                ans.add(arr[i]);
+            }
+            
+            arr = ans.stream().mapToInt(x->x).toArray();
         }
         return arr;
     }
