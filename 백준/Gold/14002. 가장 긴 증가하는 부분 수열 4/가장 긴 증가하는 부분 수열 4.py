25fb0d2dcf92622bda317df80619515e74e49bf5
이현희 <jhc03032@naver.com>
2026-03-25 15:42:41 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 96 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.py
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.py
@@ -1,27 +1,28 @@
-
 N = int(input())
 lst = [0] + list(map(int,input().split()))
 
 dp = [0] * (N+1)
-par = [0] * (N+1) # 부모 인덱스 저장 배열
 
 for i in range(1,N+1) : # 확인해야할 수
     mx = 0
     for j in range(0,i) :  # 비교 구간
         if lst[i] > lst[j] :
-            if dp[j] + 1 > dp[i] : # 최대길이가 더 긴경우
-                dp[i] = dp[j] + 1
-                par[i] = j
+            mx = max(mx,dp[j])
+    dp[i] = mx + 1
 
 ans = max(dp)
-ans_lst = []
+ans_lst = [0] * ans 
 
-idx = dp.index(ans) # 최대값이 저장되어있는 인덱스
-for _ in range(ans) :
-    ans_lst.append(lst[idx])
-    idx = par[idx]
+idx = ans
+for k in range(N, -1,-1) :
+    if dp[k] == idx :
+        ans_lst[idx-1] = lst[k]
+        idx -= 1
 
-ans_lst.reverse()
+        if idx == 0 :
+            break
 
+        
+    
 print(ans)
 print(*ans_lst)
