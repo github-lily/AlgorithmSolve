61a7909f858b85fe64791d2e58e813c1477e6f6a
이현희 <jhc03032@naver.com>
2026-03-29 01:04:15 +0900

    [Silver V] Title: 반지, Time: 100 ms, Memory: 14356 KB -BaekjoonHub

--- 백준/Silver/5555. 반지/반지.java
+++ 백준/Silver/5555. 반지/반지.java
@@ -1,44 +1,24 @@
-
 import java.io.*;
-import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String text = br.readLine();
-        int N = Integer.parseInt(br.readLine());
+        String target = br.readLine();
+        int n = Integer.parseInt(br.readLine());
+
         int ans = 0;
 
-
-        for (int i = 0; i < N; i++) {
+        for (int tc = 0; tc < n; tc++) {
             String ring = br.readLine();
-            String doubled = ring + ring; // 원형 처리
+            String doubled = ring+ring;     // 원형 반지임을 고려(처음과 끝이 만남)
 
-            boolean isRing = false;
-
-            for (int j = 0; j < ring.length(); j++) {
-                int t = 0;
-
-                while (t < text.length()) {
-                    if (doubled.charAt(j + t) != text.charAt(t)) {
-                        break;
-                    }
-                    t++;
-                }
-
-                // 전부 일치
-                if (t == text.length()) {
-                    isRing = true;
-                    break;
-                }
-
+            if (doubled.contains(target)) {
+                ans++;
             }
-            if (isRing) ans++;
-        }
 
+        }
         System.out.println(ans);
     }
 
 }
-
