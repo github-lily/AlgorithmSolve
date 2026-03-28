975f1dba98c01e513e2bed2934c244944c1b904b
이현희 <jhc03032@naver.com>
2026-03-28 15:41:18 +0900

    [Silver I] Title: 미로 탐색, Time: 124 ms, Memory: 112084 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,26 +1,25 @@
 from collections import deque
 
-def bfs(si,sj,scnt) :
-    q = deque([(si,sj,scnt)])
-    miro[si][sj] = 0
+def bfs(si,sj) :
+    q = deque([(si,sj)])
 
     while q :
-        ci,cj,cnt = q.popleft()
+        ci,cj = q.popleft()
 
         if ci == N-1 and cj == M-1 :
-            return cnt
+            return 
 
         for k in range(4) :
             ni,nj = ci+di[k], cj+dj[k]
             if 0 <= ni < N and 0 <= nj < M and miro[ni][nj] == 1 :
-                miro[ni][nj] = 0
-                q.append((ni,nj,cnt+1))
+                miro[ni][nj] = miro[ci][cj]+1
+                q.append((ni,nj))
     
 
 N, M = map(int,input().split())
 miro = [list(map(int,input())) for _ in range(N)]
 di,dj = [0,1,0,-1],[1,0,-1,0]   # 우하좌상
 
-ans = bfs(0,0,1)
+bfs(0,0)
 
-print(ans)
+print(miro[N-1][M-1])
