028cf2a4b9b1161953e98a896593eb03e24d9342
이현희 <jhc03032@naver.com>
2026-03-30 21:42:37 +0900

    [Gold V] Title: 컨베이어 벨트 위의 로봇, Time: 404 ms, Memory: 110960 KB -BaekjoonHub

--- 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.py
+++ 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.py
@@ -1,41 +1,43 @@
+
 # 0. 입력값
 N,K = map(int,input().split())
+belt = list(map(int,input().split()))
+
+robot = [False] * N
 size = N*2
-
-durability = list(map(int,input().split()))
-robot = [0] * size
 stage = 0
-s = 0       # 올리는 곳
-e = N-1     # 내리는 곳
 
-while True : 
-    # 1. 벨트 한바퀴 회전
-    s = (s-1) % size
-    e = (e-1) % size
+while True :
     stage += 1
 
+    # 1-1. 벨트 한바퀴 회전
+    temp = belt[size-1]
+    for i in range(size-1,0,-1) :
+        belt[i] = belt[i-1]
+    belt[0] = temp
+
+    # 1-2. 로봇 함께 회전
+    for i in range(N-1, 0, -1) :
+        robot[i] = robot[i-1]
+    robot[0] = False        
+    robot[N-1] = False      # 끝에 도착하면 로봇 빼기
+
     # 2. 가장 먼저 벨트에 올라간 로봇부터, 벨트가 회전하는 방향으로 한 칸 이동(이동칸에 로봇이 없고, 내구도가 1 이상일 경우에만 이동)
-    robot[e] = 0        # 로봇 내리기
+    for i in range(N-1, 0, -1) :
+        if robot[i-1] and robot[i] == False and belt[i] > 0 :
+            robot[i] = robot[i-1]
+            robot[i-1] = False
+            belt[i] -= 1
+    
+    robot[N-1] = False      # 끝에 도착하면 로봇 빼기
 
-    for i in range(N-1,0,-1) :
-        cr = (s+i) % size
-        if robot[cr] == 1 :
-            # 로봇 있는지 & 내구도 확인
-            nr = (cr+1) % size     # 다음칸 인덱스
-            if robot[nr] == 0 : 
-                if durability[nr] > 0 :
-                    robot[nr] = 1       # 로봇 이동
-                    robot[cr] = 0
-                    durability[nr] -= 1     # 내구도 감소
-
-    robot[e] = 0    # 이동 후 로봇 내리기
-        
     # 3. 로봇 올리기 (내구도 1이상일 경우)
-    if robot[s] == 0 and durability[s] > 0 :
-        robot[s] = 1
-        durability[s] -= 1
+    if belt[0] > 0 :
+        robot[0] = True
+        belt[0] -= 1
 
     # 4. 내구도가 0인 칸의 개수가 K개 이상이면 종료, 아니면 1번으로 이동 
-    if durability.count(0) >= K :
-        print(stage)
+    if belt.count(0) >= K :
         break
+
+print(stage)
