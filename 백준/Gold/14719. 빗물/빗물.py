6f876d79c31ec41d98b6fb9d44239220ecaf001a
이현희 <jhc03032@naver.com>
2026-04-04 18:59:31 +0900

    [Gold V] Title: 빗물, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Gold/14719. 빗물/빗물.py
+++ 백준/Gold/14719. 빗물/빗물.py
@@ -2,15 +2,21 @@ H,W = map(int,input().split())
 walls = list(map(int,input().split()))
 
 ans = 0
+l,r = 0, W-1
+l_mx, r_mx = 0,0
 
-for i in range(1,W) :
-    left = max(walls[:i])
-    right = max(walls[i:])
+while l < r :
+    if walls[l] < walls[r] :        # 더 낮은 왼쪽 벽이 기준
+        if walls[l] > l_mx :
+            l_mx = walls[l]         # 왼쪽 벽 갱신
+        else :
+            ans += l_mx - walls[l]
+        l += 1
+    else :      # 더 낮은 오른쪽 벽이 기준
+        if walls[r] > r_mx :
+            r_mx = walls[r]         # 오른쪽 벽 갱신
+        else :
+            ans += r_mx - walls[r]
+        r -= 1
 
-    water = min(left,right) - walls[i]
-    if water < 0 :
-        water = 0
-
-    ans += water
-
-print(ans) 
+print(ans)
