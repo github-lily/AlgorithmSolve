0ac9a5d783a158e91cfce8eedd2a78ff1429da1a
이현희 <jhc03032@naver.com>
2026-04-06 20:56:56 +0900

    [Silver I] Title: 지름길, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/1446. 지름길/지름길.py
+++ 백준/Silver/1446. 지름길/지름길.py
@@ -1,13 +1,19 @@
+
 import sys
 input = sys.stdin.readline
 
 N, D = map(int,input().split())
 dp = [x for x in range(D+1)]        # 해당 index까지의 최단거리 기록
 
-shortcuts = [list(map(int,input().split())) for _ in range(N)]
+shortcuts = []
+for _ in range(N) :
+    s,e,l = map(int,input().split())
+    # 도착점이 끝점보다 멀거나, 길이가 원래 이동거리보다 먼 경우 제외
+    if e <= D and e-s > l :
+        shortcuts.append([s,e,l])
 shortcuts.sort()
 
-s = 0
+s_idx = 0
 
 for i in range(D+1) :
     # i를 1부터 시작하면 지름길 시작이 0인경우 체크가 안됨
@@ -16,11 +22,11 @@ for i in range(D+1) :
             dp[i] = dp[i-1] + 1
 
     # 지름길 체크(같은 시작점이 여러개일 수 있으므로 계속 체크)
-    while s < len(shortcuts) and shortcuts[s][0] == i :
-        start,end,length = shortcuts[s]
+    while s_idx < len(shortcuts) and shortcuts[s_idx][0] == i :
+        start,end,length = shortcuts[s_idx]
         if end <= D : 
             if dp[end] > dp[i] + length :
                 dp[end] = dp[i] + length
-        s += 1
+        s_idx += 1
 
 print(dp[D])
