db5e0fdf24f8c57ba0223b9c07506f69b11195b3
이현희 <jhc03032@naver.com>
2026-04-13 20:53:35 +0900

    [Silver III] Title: 영단어 암기는 괴로워, Time: 252 ms, Memory: 136472 KB -BaekjoonHub

--- 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.py
+++ 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.py
@@ -1,24 +1,24 @@
-
 import sys
 input = sys.stdin.readline
 
 N, M = map(int,input().split())
-dict = {}
+word_cnt = {}
 
 # 사전에 단어 저장
 for _ in range(N) :
     word = input().strip()
     if len(word) >= M :
-        dict[word] = dict.get(word,0)+1
+        word_cnt[word] = word_cnt.get(word,0)+1
 
-ans = list(dict.keys())
+words = list(word_cnt.keys())
 
-'''
-정렬 조건
-1. 자주 나오는 단어일수록 앞에 배치한다.
-2. 해당 단어의 길이가 길수록 앞에 배치한다.
-3. 알파벳 사전 순으로 앞에 있는 단어일수록 앞에 배치한다
-'''
-ans.sort(key = lambda x : (-dict[x], -len(x),x))
+# 알파벳 사전 순으로 앞에 있는 단어일수록 앞에 배치한다. (3순위)
+words.sort()
 
-print("\n".join(ans))
+# 해당 단어의 길이가 길수록 앞에 배치한다. (2순위)
+words.sort(key = len, reverse= True)
+
+# 자주 나오는 단어일수록 앞에 배치한다. (1순위)
+words.sort(key = word_cnt.get, reverse=True)
+
+print("\n".join(words))
