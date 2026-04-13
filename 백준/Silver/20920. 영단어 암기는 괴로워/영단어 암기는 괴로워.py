cbde25e06fc893325050c8784e9627169d0459fa
이현희 <jhc03032@naver.com>
2026-04-13 20:47:57 +0900

    [Silver III] Title: 영단어 암기는 괴로워, Time: 372 ms, Memory: 135144 KB -BaekjoonHub

--- 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.py
+++ 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.py
@@ -1,3 +1,4 @@
+
 import sys
 input = sys.stdin.readline
 
@@ -8,31 +9,16 @@ dict = {}
 for _ in range(N) :
     word = input().strip()
     if len(word) >= M :
-        if word in dict.keys() :
-            dict[word] += 1
-        else :
-            dict[word] = 1
-    
-# 1. 자주 나오는 단어일수록 앞에 배치한다.
-cnt = len(dict)
-mx_cnt = max(dict.values())
-words = [[] for _ in range(mx_cnt+1)]
+        dict[word] = dict.get(word,0)+1
 
-for word,val in dict.items() :
-    words[val].append(word)
+ans = list(dict.keys())
 
-ans = []
-
-for i in range(mx_cnt,-1,-1) :
-    lst = words[i]
-    if lst :
-        if len(lst) == 1 :
-            ans.append(lst[0])
-        else :
-            # 2. 해당 단어의 길이가 길수록 앞에 배치한다.
-            # 3. 알파벳 사전 순으로 앞에 있는 단어일수록 앞에 배치한다.
-            lst.sort(key = lambda x:(-len(x),x)) 
-            ans.extend(lst)
-
+'''
+정렬 조건
+1. 자주 나오는 단어일수록 앞에 배치한다.
+2. 해당 단어의 길이가 길수록 앞에 배치한다.
+3. 알파벳 사전 순으로 앞에 있는 단어일수록 앞에 배치한다
+'''
+ans.sort(key = lambda x : (-dict[x], -len(x),x))
 
 print("\n".join(ans))
