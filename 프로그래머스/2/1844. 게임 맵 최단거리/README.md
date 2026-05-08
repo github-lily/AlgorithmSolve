034fc9bce2fc2ff56edff13690f50155d7c8a92b
이현희 <jhc03032@naver.com>
2026-05-08 22:24:12 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.70 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844#) 
 
 ### 성능 요약
 
-메모리: 9.27 MB, 시간: 10.78 ms
+메모리: 9.2 MB, 시간: 7.70 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 05월 08일 22:16:44
+2026년 05월 08일 22:24:10
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -6,20 +6,21 @@ def solution(maps):
     di,dj = [0,1,0,-1],[1,0,-1,0]
     
     v = [[0] * m for _ in range(n)]
-    q = deque([(0,0,0)])
+    q = deque([(0,0,1)])
 
     v[0][0] = 1
     
     while q :
         ci,cj,cnt = q.popleft()
-        cnt += 1
+
         if ci == n-1 and cj == m-1 :
             return cnt
+        
         for k in range(4)  :
             ni,nj = ci + di[k], cj + dj[k]
             if 0<= ni < n and 0<= nj < m :
-                if v[ni][nj] == 0 and maps[ni][nj] == 1:
-                    q.append((ni,nj,cnt))
+                if v[ni][nj] == 0 and maps[ni][nj] == 1 :
+                    q.append((ni,nj,cnt+1))
                     v[ni][nj] = 1
                     
     return -1
