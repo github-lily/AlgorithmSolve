3b37670e8263208d08acdb18fde2b99f23ed8514
이현희 <jhc03032@naver.com>
2026-05-12 12:57:36 +0900

    [level 3] Title: 단어 변환, Time: 0.12 ms, Memory: 9.16 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -1,21 +1,39 @@
 from collections import deque
+# 가능한 단어를 q에 넣고 방문 표시 하며 비교
+
+def check(now, nxt) :
+    diff = 0
+    for i in range(len(now)) :
+        if diff > 1 :
+            return False
+        if now[i] != nxt[i] :
+            diff += 1
+    if diff == 1 :
+        return True     # 하나만 다르면 통과
+    else :
+        return False    # 같은 단어는 거르기
 
 def solution(begin, target, words):
-    if target not in words:
+    
+    if target not in words :
         return 0
     
-    q = deque([(begin, 0)])
+    v = set()
+    q = deque([(begin,0)])
+            
     
-    while q:
-        word, cnt = q.popleft()
-        if word == target:
-            return cnt
+    while q :
+        cur, count = q.popleft()
+        if cur == target :
+            return count
         
-        for w in words:
-            diff = sum(a != b for a, b in zip(word, w))
-            if diff == 1:
-                q.append((w, cnt + 1))
-                words.remove(w)
-                break
-                
+        for word in words :
+            if word not in v and check(cur,word) :
+                q.append((word,count+1))
+                v.add(word)
+
+    
     return 0
+            
+    
+    
