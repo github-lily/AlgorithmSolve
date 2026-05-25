8ad5ea46bcb9f4321bd9688baabae1c6cda0a8b3
이현희 <jhc03032@naver.com>
2026-05-25 20:44:58 +0900

    [level 3] Title: 아이템 줍기, Time: 1.05 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.py
+++ 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.py
@@ -1,43 +1,42 @@
 from collections import deque
 
-def solution(rectangle, characterX, characterY, itemX, itemY):
-    answer = 0
-
-    field = [[-1] * 102 for i in range(102)]
+def solution(rect, ux, uy, ix, iy):
+    # ㄷ,ㅁ 경우 고려 2배처리
+    arr = [[-1]*102 for _ in range(102)]
+    v = [[-1]*102 for _ in range(102)]
     
-    for r in rectangle:
-    	# 모든 좌표값 2배
-        x1, y1, x2, y2 = map(lambda x: x*2, r)
-        for i in range(x1, x2+1):
-            for j in range(y1, y2+1):
-                if x1 < i < x2 and y1 < j < y2:
-                    field[i][j] = 0
-                # 다른 직사각형의 내부가 아니면서 현재 직사각형의 테두리일 때 1로 채움
-                elif field[i][j] != 0:
-                    field[i][j] = 1
+    di, dj = [0,1,0,-1], [1,0,-1,0]
+    
+    for r in rect :
+        sx,sy,ex,ey = map(lambda x : x*2, r)
+        # 내부 빈공간 처리
+        for i in range(sy+1,ey) :
+            for j in range(sx+1,ex) :
+                arr[i][j] = 0
+        # 테두리 표시
+        for i in range(sy,ey+1) :
+            for j in range(sx,ex+1) :
+                if arr[i][j] != 0 :
+                    arr[i][j] = 1
                     
-    dx = [-1, 1, 0, 0]
-    dy = [0, 0, -1, 1]
     
-    q = deque()
-    q.append([characterX * 2, characterY * 2])
-    visited = [[1] * 102 for i in range(102)] # 아직 방문하지 않은 곳은 1로 표시
-    visited[characterX * 2][characterY * 2] = 0 # 시작 지점은 0으로 초기화
+    q = deque([(uy*2, ux*2)])
+    v[uy*2][ux*2] = 0
     
-    while q:
-        x, y = q.popleft()
-        # 도착한 곳이 아이템이 있는 장소라면 현재의 최단거리(나누기 2)를 answer로 하고 while문을 빠져나옴
-        if x == itemX * 2 and y == itemY * 2:
-            answer = visited[x][y] // 2
-            break
-        # 아니라면 상하좌우를 탐색
-        for k in range(4):
-            nx = x + dx[k]
-            ny = y + dy[k]
-            
-            # 현재 좌표가 테두리이고 아직 방문하지 않은 곳이라면 q에 추가 후 visited를 최단거리로 갱신
-            if field[nx][ny] == 1 and visited[nx][ny] == 1:
-                q.append([nx, ny])
-                visited[nx][ny] = visited[x][y] + 1
+    while q :
+        ci, cj = q.popleft()
+        # 아이템 위치 도착하면 종료
+        if ci == iy*2 and cj == ix*2 :
+            ans = v[ci][cj] // 2
+            return ans
+        
+        
+        for k in range(4) :
+            ni,nj = ci+di[k], cj+dj[k]
+            if 0 <= ni < 102 and 0 <= nj < 102 :
+                if arr[ni][nj] == 1 and v[ni][nj] == -1 :
+                    v[ni][nj] = v[ci][cj] + 1       # 지나온 칸 표시
+                    q.append((ni,nj))
     
-    return answer
+
+        
