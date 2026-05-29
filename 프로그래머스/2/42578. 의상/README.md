99ae5f699ae13dec4939336f76e6a00a2af121e3
이현희 <jhc03032@naver.com>
2026-05-29 09:34:51 +0900

    [level 2] Title: 의상, Time: 0.01 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/README.md
+++ 프로그래머스/2/42578. 의상/README.md
@@ -1,10 +1,10 @@
 # [level 2] 의상 - 42578 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42578#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42578) 
 
 ### 성능 요약
 
-메모리: 9.35 MB, 시간: 0.04 ms
+메모리: 11.4 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 10월 09일 12:36:56
+2026년 05월 29일 09:34:46
 
 ### 문제 설명
 
--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,21 +1,20 @@
-from collections import defaultdict
-
 def solution(clothes):
-    closet = defaultdict(int)
+    # 종류별 옷 개수 카운트
+    cnt = dict()
+    for cloth,typ in clothes :
+        if typ not in cnt :
+            cnt[typ] = 1
+        else :
+            cnt[typ] += 1
     
-    for cloth in clothes :
-        closet[cloth[1]] += 1
+    # A 3개 B 2개 C 4개 있을 때 경우의 수
+    # 각각 N+1개(아무것도 안고른 경우) - ABC 중 아무것도 안 고른 경우의 수 1 
+    # (4 * 3 * 5) - 1  = 59개
     
-    print(closet)
-    
-    if len(closet) == 1 :
-        key,val = closet.popitem()
-        return val
-    
+    vals = cnt.values()
     ans = 1
-    for i in closet.values() :
-        ans *= i+1      # 입는경우(옷의종류개수) + 안입는경우(1)
     
-    return ans-1
+    for val in vals :
+        ans *= (val+1)
         
-        
+    return ans - 1      
