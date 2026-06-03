7d7d085328dda5555c1b4b557ff94a3f9beadf6e
이현희 <jhc03032@naver.com>
2026-06-03 23:54:44 +0900

    [level 2] Title: 가장 큰 수, Time: 48.60 ms, Memory: 22.8 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,7 +1,14 @@
 def solution(numbers):
-    numbers = list(map(str,numbers))
-    numbers.sort(key = lambda x : x*3, reverse=True)
-    answer = str(int(''.join(numbers)))
+    nums = list(map(str,numbers))
+    
+    # 파이썬 문자열 비교의 특징! 한글자씩 비교하기 때문에 333 > 303030
+    nums.sort(key = lambda x : x*3, reverse = True)
+    
+    # [0,0,0] 이면 000이 나오니까 0이 나오도록 str -> int or 확인
+    ans = ''.join(nums)
+    
+    if ans[0] == '0' :
+        ans = '0'
+    return ans
     
     
-    return answer
