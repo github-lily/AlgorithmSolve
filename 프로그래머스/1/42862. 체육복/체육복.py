2247038125d53a3d5df50a87619ea8756842f843
이현희 <jhc03032@naver.com>
2026-06-05 12:01:00 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -1,15 +1,33 @@
 def solution(n, lost, reserve):
-    los = set(lost) - set(reserve)
-    res = set(reserve) - set(lost)
-    
-    ans = n - len(los)
-
-    for l in los :
-        if l-1 in res :
-            res.remove(l-1)
-            ans += 1
-        elif l+1 in res :
-            res.remove(l+1)
-            ans += 1
+    lst = [1] * (n+2)
         
+    # 잃어버린거 체크
+    for l in lost :
+        lst[l] -= 1
+    
+    # 여벌 체크
+    for r in reserve :
+        lst[r] += 1
+    
+    ans = 0
+    for i in range(1, n+1) :
+        if lst[i] == 1 :
+            ans += 1        # 본인 것만 있으면 넘어감   
+        # 여분 있을 때
+        elif lst[i] > 1 :
+            # 이전 사람이 체육복 없다면
+            if i != 1 and lst[i-1] == 0 :
+                lst[i] -= 1
+                lst[i-1] += 1
+                ans += 1
+            # 뒷 사람이 체육복 없다면
+            if i != n+1 and lst [i] > 1 :
+                if lst[i+1] == 0 :
+                    lst[i] -= 1
+                    lst[i+1] += 1
+            ans += 1
+        else :
+            continue
+    
     return ans
+        
