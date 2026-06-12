8f567cdbf8183a1e0b8ace941ecb9f2eac6c2b2d
이현희 <jhc03032@naver.com>
2026-06-13 01:00:36 +0900

    [level 2] Title: 주식가격, Time: 17.19 ms, Memory: 17 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,16 +1,28 @@
 def solution(prices):
-    n = len(prices)
-    ans = [0] * n
-    st = []  # index stack
-
-    for t, p in enumerate(prices):
-        while st and prices[st[-1]] > p:
-            i = st.pop()
-            ans[i] = t - i
-        st.append(t)
-
-    while st:
-        i = st.pop()
-        ans[i] = (n - 1) - i
-
+    N = len(prices)
+    stack = []
+    ans = [0] * N
+    
+    for i in range(N-1,-1,-1) :
+        now = prices[i]
+        
+        # 현재 가격보다 크거나 같은 값은 하락 지점이 될 수 없으므로 제거
+        while stack and prices[stack[-1]] >= now :
+            stack.pop()
+            
+        # 끝까지 작은 값이 안나온 경우
+        if not stack :
+            ans[i] = N-1-i
+        
+        # 처음으로 작은 값을 만난 경우
+        else :
+            ans[i] = stack[-1] - i
+            
+        
+        # 현재 인덱스 추가
+        stack.append(i)
+        
     return ans
+
+                
+            
