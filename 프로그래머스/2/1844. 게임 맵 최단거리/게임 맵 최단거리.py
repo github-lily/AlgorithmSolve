075863cf596f6a56c109210e3a1eba2ef64bebc0
이현희 <jhc03032@naver.com>
2026-06-14 23:39:10 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 8.82 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,26 +1,42 @@
 from collections import deque
 
+# 시작과 종료 블럭 포함
 def solution(maps):
     n = len(maps)
     m = len(maps[0])
-    di,dj = [0,1,0,-1],[1,0,-1,0]
     
-    v = [[0] * m for _ in range(n)]
-    q = deque([(0,0,1)])
-
-    v[0][0] = 1
-    
-    while q :
-        ci,cj,cnt = q.popleft()
-
-        if ci == n-1 and cj == m-1 :
-            return cnt
+    def bfs(si,sj) :
+        di, dj = [1,0,-1,0],[0,1,0,-1]  # 하우상좌
+        v = [[0] * m for _ in range(n)]
+        v[si][sj] = 1
+        q = deque([(si,sj)])
+        cnt = 0
         
-        for k in range(4)  :
-            ni,nj = ci + di[k], cj + dj[k]
-            if 0<= ni < n and 0<= nj < m :
-                if v[ni][nj] == 0 and maps[ni][nj] == 1 :
-                    q.append((ni,nj,cnt+1))
-                    v[ni][nj] = 1
-                    
-    return -1
+        while q :
+            ci,cj = q.popleft()
+            
+            # 도착시 종료
+            if ci == n-1 and cj == m-1 :
+                return v[ci][cj]
+
+            for k in range(4) :
+                ni,nj = ci+di[k], cj+dj[k]
+                # 범위 내 & 벽 아님
+                if 0 <= ni < n and 0 <= nj < m and maps[ni][nj] == 1 :
+                    # 방문 안함 or 더 가까운 경로일 떄 이동
+                    if v[ni][nj] == 0 or v[ni][nj] > v[ci][cj] + 1 :
+                        q.append((ni,nj))
+                        v[ni][nj] = v[ci][cj] + 1
+        return -1
+        
+                
+    ans = bfs(0,0)
+    
+    return ans
+
+    
+    
+
+    
+    
+    
