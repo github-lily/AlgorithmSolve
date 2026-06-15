6d61117c39c2e48c2bb297e43cc4b903e3e92eeb
이현희 <jhc03032@naver.com>
2026-06-15 21:31:39 +0900

    [level 2] Title: 할인 행사, Time: 24.28 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.py
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.py
@@ -1,15 +1,21 @@
 def solution(want, number, discount):
-    # 가지치기 : 원하는게 할인 안하면 종료
+    # 가지치기 1 : 원하는게 할인 안하면 종료
     if len(set(want) - set(discount)) > 0 :
         return 0
     
     ans = 0
+    N = sum(number)
+    D = len(discount)
+    
+    # 가지치기 2 : 할인일이 원하는 구매 개수보다 적으면 종료
+    if N > D :
+        return 0
+    
     
     # want 저장
     needs = {}
-    N = sum(number)
     for i in range(len(number)) :
-        needs[want[i]] = needs.get(want[i], number[i])
+        needs[want[i]] = number[i]
     
     def check(dic) :
         vals = dic.values()
@@ -29,26 +35,21 @@ def solution(want, number, discount):
     if check(needs) :
         ans += 1
 
-
-
     # 범위 옮겨가며 확인
-    D = len(discount)
-    for i in range(D) :
+    for i in range(D-N) :
 
-        de = discount[i]
         # 맨 앞 물건 삭제
+        de = discount[i]
         if de in needs :
             needs[de] += 1
-        if i+N < D :
-            ad = discount[i+N]
-            if ad in needs :
-                needs[ad] -= 1
-
         
+        # 뒷 물건 추가
+        ad = discount[i+N]
+        if ad in needs :
+            needs[ad] -= 1
+
         if check(needs) :
             ans += 1
-            print(i, i+N)
-            print(needs)
             
     return ans
         
