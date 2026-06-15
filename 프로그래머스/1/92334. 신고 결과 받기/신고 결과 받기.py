bc5aee2a6abb638cb29657c39337944ffca8f336
이현희 <jhc03032@naver.com>
2026-06-15 19:10:37 +0900

    [level 1] Title: 신고 결과 받기, Time: 656.87 ms, Memory: 39.9 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.py
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.py
@@ -1,29 +1,16 @@
-# 신고 하기
-# K번 이상 신고당하면 정지, 메일 발송
-# 결과 메일 수
-
 def solution(id_list, report, k):
-    report_recive = {}
-    mail_recive = {}
+    # report [0] : 신고자, [1] 신고 당한 사람
+    ans = [0] * len(id_list)
+    reports = {x:0 for x in id_list}
     
-    for id in id_list :
-        report_recive[id] = set()
-        mail_recive[id] = 0
-        
-    for rep in report :
-        send, recive = rep.split()
-        report_recive[recive].add(send)
+    # 신고 당한 횟수 카운트
+    for r in set(report) :
+        reports[r.split()[1]] += 1
     
-
-    for name, sends in report_recive.items() :
-        if len(sends) >= k :
-            for s in sends :
-                mail_recive[s] += 1
+    # 메일 발송 횟수 카운트
+    for r in set(report) :
+        send, recive = r.split()
+        if reports[recive] >= k :
+            ans[id_list.index(send)] += 1
     
-    ans = []
-    for id in id_list :
-        ans.append(mail_recive[id])
-        
     return ans
-            
-    
