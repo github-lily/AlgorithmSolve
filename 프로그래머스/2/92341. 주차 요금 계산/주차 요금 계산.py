c9f4cceddb6ee9b44fb9cb09296289ac48c4acae
이현희 <jhc03032@naver.com>
2026-06-15 21:55:36 +0900

    [level 2] Title: 주차 요금 계산, Time: 1.57 ms, Memory: 12 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.py
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.py
@@ -1,69 +1,51 @@
 import math
+# 차량별 누적 주차 시간을 계산하여 요금 일괄 정산
+# 초과한 시간은 올림
+# 차량 번호 기준 오름차순
 
 def solution(fees, records):
-
-    # 시간 계산 함수
-    def time_cal(start,end) :
-        sh,sm = map(int, start.split(':'))
-        eh,em = map(int, end.split(':'))
-        
-        start_min = sh * 60 + sm
-        end_min = eh * 60 + em
-        
-        res = end_min - start_min
-        
-        return res
-    
-    
-    # 주차 요금 계산 함수
-    def fee_cal(t) :
-        fee = fees[1]
-        # 기본요금[1] + ((이용시간 - 기본시간[0])/단위시간[2]) * 단위요금[3]
-        if t > fees[0] :    # 기본시간 이상 이용 시에만 계산
-            fee += (math.ceil((t - fees[0]) / fees[2])) * fees[3]
-        return fee
-    
-    # 차량별 기록 저장
     cars = {}
+    results = []
     
-    for record in records :
-        time, num, rec = record.split()
-        if num not in cars :
-            cars[num] = []
-        cars[num].append(time)
-    
-    # 차량별 요금 계산
-    total_fees = []
-    for car in cars :
-        tmp = cars[car]
-        money = 0
-        total_time = 0
-        N = len(tmp)
+    # 분으로 변환
+    def to_min(t) :
+        h,m = map(int,t.split(":"))
+        return h*60 + m
         
-        # 출차 기록 있을 때
-        if N % 2 == 0 : 
+    # 차량 시간 기록
+    for record in records :
+        time, car, typ = record.split()
+        mn = to_min(time)
+        if car not in cars :
+            cars[car] = []
+        cars[car].append((typ, mn))
+    
+    # 누적 시간 & 요금 계산
+    for key, vals in cars.items() :
+        temp = 0
+        # 입출차 모두 이루어짐
+        N = len(vals)
+        if N % 2 == 0 :
             for i in range(1,N,2) :
-                total_time += time_cal(tmp[i-1], tmp[i])
-
-                
-        # 출차 기록 없을 때
+                temp += vals[i][1] - vals[i-1][1]
+        # 출차 없음
         else :
             for i in range(1,N-1,2) :
-                total_time += time_cal(tmp[i-1], tmp[i])
-            total_time += time_cal(tmp[N-1], '23:59')
+                temp += vals[i][1] - vals[i-1][1]
+            # 23:59 기준 계산
+            end = 23*60 + 59
+            temp += end - vals[-1][1]
         
-        # 누적 시간으로 요금 계산
-        money += fee_cal(total_time) 
-        total_fees.append([car, money])
+        # fee = 기본요금[1] + ceil((누적 시간 - 기본 시간[0]) / 단위 시간[2]) * 단위 요금[3]
+        if temp <= fees[0] :        # 기본시간 미만 사용
+            fee = fees[1]
+        else :
+            fee = fees[1] + math.ceil((temp - fees[0]) / fees[2]) * fees[3]
+            
+            
+        results.append((key, fee))
     
-    # 정렬 :차량 번호 올림차순
-    total_fees.sort()
-    result = []
-    for total_fee in total_fees :
-        result.append(total_fee[1])
-    
-    return result
-                
-                
-        
-    
+    # 정답
+    results.sort()
+    ans = [x[1] for x in results]
+    return ans
