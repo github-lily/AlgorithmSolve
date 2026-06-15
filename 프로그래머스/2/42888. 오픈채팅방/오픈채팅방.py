09b596b41c8b769ef20053f1c924e841d22f43c2
이현희 <jhc03032@naver.com>
2026-06-15 19:27:35 +0900

    [level 2] Title: 오픈채팅방, Time: 81.62 ms, Memory: 49.1 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.py
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.py
@@ -1,23 +1,31 @@
-def solution(records):
-    users = {}
+def solution(record):
+    udict = {}
+    orders = []
     result = []
-    for record in records :
-        rec = record.split()
-        act, uid = rec[0], rec[1]
-        if act == 'Enter' :
-            name = rec[2]
-            users[uid] = name
-            result.append((uid, '님이 들어왔습니다.'))
-        elif act == 'Change' :
-            name = rec[2]
-            users[uid] = name
+    
+    # 닉네임 저장
+    for rec in record :
+        if rec.split()[0] == 'Leave' :
+            orders.append(('L', rec.split()[1]))
         else :
-            result.append((uid, '님이 나갔습니다.'))
+            com, uid, name = rec.split()
+            if com == 'Enter' :
+                udict[uid] = name
+                orders.append(('E', uid))
+
+            else :  # Change
+                udict[uid] = name
     
-    ans = []
-    for res in result :
-        ans.append(users[res[0]] + res[1] )
-    
-    return ans
+    # 메시지 생성
+    for order in orders :
+        typ, uid = order
+        if typ == 'E' :
+            result.append(f'{udict[uid]}님이 들어왔습니다.')
+        else :
+            result.append(f'{udict[uid]}님이 나갔습니다.')
             
+    return result
         
+            
+            
+            
