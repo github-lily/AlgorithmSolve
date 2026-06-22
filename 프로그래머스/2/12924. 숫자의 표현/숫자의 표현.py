16013d88223948c0df05c940122ac94bda07260f
이현희 <jhc03032@naver.com>
2026-06-22 13:53:09 +0900

    [level 2] Title: 숫자의 표현, Time: 3.29 ms, Memory: 11.2 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
@@ -2,11 +2,9 @@ def solution(n):
     ans = 0
     
     # 누적합 리스트 만들기
-    lst = [x for x in range(n+1)]
     prefix = [0] * (n+1)
     for i in range(1,n+1) :
-        prefix[i] = prefix[i-1] + lst[i] 
-    print(prefix)
+        prefix[i] = prefix[i-1] + i
     
     # 투포인터로 탐색
     head = 1
@@ -14,7 +12,6 @@ def solution(n):
     
     while head <= n and head != tail:
         sm = prefix[head] - prefix[tail]
-        # print('head : ', prefix[head], 'tail : ', prefix[tail], 'sm : ', sm)
         if sm == n :
             head += 1
             ans += 1
@@ -22,7 +19,7 @@ def solution(n):
             tail += 1
             if tail >= head :
                 head += 1
-        else :  # sm < m
+        else :  # sm < n
             head += 1
         
     
