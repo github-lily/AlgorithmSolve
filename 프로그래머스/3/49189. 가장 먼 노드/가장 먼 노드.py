d63f6397a562ad8474db4ce9b41f395402e2dc02
이현희 <jhc03032@naver.com>
2026-06-24 22:52:54 +0900

    [level 3] Title: 가장 먼 노드, Time: 16.72 ms, Memory: 21.9 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
@@ -1,35 +1,32 @@
+# 1번 노드로부터 가장 멀리 떨어진 노드의 개수
+# 양방향
 from collections import deque
 
-def solution(n, edge):
-    graph = [[] for _ in range(n+1)]
-    v = [0] * (n+1)
-
+def solution(n, vertex):
+    graph = [[] for _ in range(n+1)]    # 1-index
     
-    for a,b in edge :
+    for a,b in vertex :
         graph[a].append(b)
         graph[b].append(a)
     
-    q = deque([1]) 
-    v[1] = 1
-
-        
+    visit = [-1] * (n+1)        # 거리 기록 방문배열
+    start = 1
+    
+    
+    q = deque([start])
+    visit[start] = 0
     while q :
-        c = q.popleft()
+        cur = q.popleft()
         
-        for next in graph[c] :
-            if v[next] == 0 or v[next] > v[c] +1 :
-                v[next] = v[c]+1
-                q.append(next)
+        for nxt in graph[cur] :
+            if visit[nxt] == -1  :
+                visit[nxt] = visit[cur] + 1
+                q.append(nxt)
                 
-                
-    ans = 0
-    mx = max(v)
     
-    for idx in range(1,n+1) :
-        if v[idx] == mx :
-            ans += 1
-    
+    mx = max(visit)
+    ans = visit.count(mx)
     return ans
-            
+    
     
     
