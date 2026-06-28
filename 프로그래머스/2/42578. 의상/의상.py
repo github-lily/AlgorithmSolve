641484ad444ce28a23e76a02a7b547770c6ea73c
이현희 <jhc03032@naver.com>
2026-06-29 00:41:43 +0900

    [level 2] Title: 의상, Time: 0.02 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,20 +1,14 @@
 def solution(clothes):
-    # 종류별 옷 개수 카운트
-    cnt = dict()
-    for cloth,typ in clothes :
-        if typ not in cnt :
-            cnt[typ] = 1
-        else :
-            cnt[typ] += 1
-    
-    # A 3개 B 2개 C 4개 있을 때 경우의 수
-    # 각각 N+1개(아무것도 안고른 경우) - ABC 중 아무것도 안 고른 경우의 수 1 
-    # (4 * 3 * 5) - 1  = 59개
-    
-    vals = cnt.values()
+    wardrobe = dict()
     ans = 1
     
-    for val in vals :
-        ans *= (val+1)
-        
-    return ans - 1      
+    for cloth, typ in clothes :
+        if typ not in wardrobe :
+            wardrobe[typ] = []
+        wardrobe[typ].append(cloth)
+    
+    
+    for w in wardrobe.keys() :
+        ans *= (len(wardrobe[w]) + 1)
+    
+    return ans -1       # 모두 안입은 경우 하나 제외
