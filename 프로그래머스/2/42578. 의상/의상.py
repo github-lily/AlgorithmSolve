5912cfd318a77038fa280b890ccc662192c06f97
이현희 <jhc03032@naver.com>
2026-06-29 00:43:51 +0900

    [level 2] Title: 의상, Time: 0.01 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -4,11 +4,11 @@ def solution(clothes):
     
     for cloth, typ in clothes :
         if typ not in wardrobe :
-            wardrobe[typ] = []
-        wardrobe[typ].append(cloth)
+            wardrobe[typ] = 1
+        wardrobe[typ] += 1
     
     
-    for w in wardrobe.keys() :
-        ans *= (len(wardrobe[w]) + 1)
+    for w in wardrobe.values() :
+        ans *= w
     
     return ans -1       # 모두 안입은 경우 하나 제외
