6bdf8a32eb7cdcce8877aabd87c5f618e447fd01
이현희 <jhc03032@naver.com>
2026-07-02 15:25:21 +0900

    [level 3] Title: 베스트앨범, Time: 0.06 ms, Memory: 11.7 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -1,35 +1,35 @@
-from collections import defaultdict
+import heapq as hq
 
 def solution(genres, plays):
+    # 속한 노래가 많이 재생된 장르
+    # 장르 내에서 많이 재생된 노래
+    # 같다면, 고유 번호가 낮은 노래 먼저
     
-    songs = defaultdict(list)
-    total_cnt = defaultdict(int)
+    # 노래 카운트
+    cnt_dic  = dict()
+    song_dic = dict()
+    
+    for i in range(len(genres)) :
+        g = genres[i]
+        if not g in cnt_dic :
+            cnt_dic[g] = 0
+            song_dic[g] = []
+        cnt_dic[g] += plays[i]
+        song_dic[g].append((plays[i],-i))       # 고유번호 낮은 번호 먼저
+    
+    # 순서 정렬
+    order = []
+    for gen in song_dic :
+        song_dic[gen].sort(key=lambda x : (x[0], x[1]), reverse=True)
+        hq.heappush(order, (-cnt_dic[gen], gen))       # 최대힙
+    
+    # 앨범 수록
     ans = []
+    while order :
+        cur_cnt, cur_gen = hq.heappop(order)
+        rng = min(len(song_dic[cur_gen]),2)    # 원소가 하나일 수도 있으므로
+        for i in range(rng) :
+            ans.append(-song_dic[cur_gen][i][1])
     
-    for i, cnt in enumerate(plays) :
-        songs[genres[i]] += [(cnt, i)]
-        total_cnt[genres[i]] += cnt
-    
-    # 1. 장르 구하기
-    sort_cnt = sorted(total_cnt, key = lambda x : total_cnt[x], reverse = True)
-    
-    # 노래 구하기
-    for g in sort_cnt :
-        # 2. 장르 내 정렬
-        # 주의! 재생수는 내림차순, 인덱스는 오름차순이므로 reverse = True 하면 인덱스 정렬에 문제생김!
-        songs[g].sort(key=lambda x : (-x[0],x[1]))
-        g_cnt = 0
-        for song in songs[g] :
-            ans.append(song[1])
-            g_cnt += 1
-            
-            # 3. 장르별 최대 2곡 선택
-            if g_cnt == 2 :
-                break
-                
     return ans
-
-
         
-        
-    
