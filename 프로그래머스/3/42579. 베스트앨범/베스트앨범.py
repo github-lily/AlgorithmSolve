f41c780d7d1a4e0788039a89d2ffceae624410c9
이현희 <jhc03032@naver.com>
2026-07-02 15:58:59 +0900

    [level 3] Title: 베스트앨범, Time: 0.07 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -1,35 +1,27 @@
 import heapq as hq
 
 def solution(genres, plays):
-    # 속한 노래가 많이 재생된 장르
-    # 장르 내에서 많이 재생된 노래
-    # 같다면, 고유 번호가 낮은 노래 먼저
-    
-    # 노래 카운트
-    cnt_dic  = dict()
+    cnt_dic = dict()
     song_dic = dict()
     
-    for i in range(len(genres)) :
-        g = genres[i]
-        if not g in cnt_dic :
-            cnt_dic[g] = 0
-            song_dic[g] = []
-        cnt_dic[g] += plays[i]
-        song_dic[g].append((plays[i],-i))       # 고유번호 낮은 번호 먼저
+    # 개수와 노래 저장
+    for idx, (genre, play) in enumerate(zip(genres,plays)) :
+        if genre not in cnt_dic :
+            cnt_dic[genre] = 0
+            song_dic[genre] = []
+        cnt_dic[genre] += play
+        song_dic[genre].append((idx,play))
     
-    # 순서 정렬
-    order = []
-    for gen in song_dic :
-        song_dic[gen].sort(key=lambda x : (x[0], x[1]), reverse=True)
-        hq.heappush(order, (-cnt_dic[gen], gen))       # 최대힙
+    # 속한 노래가 많이 재생된 장르 정렬
+    sorted_genre = sorted(cnt_dic.keys(), key=lambda g : cnt_dic[g], reverse = True)
     
-    # 앨범 수록
     ans = []
-    while order :
-        cur_cnt, cur_gen = hq.heappop(order)
-        rng = min(len(song_dic[cur_gen]),2)    # 원소가 하나일 수도 있으므로
-        for i in range(rng) :
-            ans.append(-song_dic[cur_gen][i][1])
-    
+    for genre in sorted_genre :
+        # 장르 내 재생 횟수, idx별 정렬
+        sorted_song = sorted(song_dic[genre], key = lambda x : (x[1], -x[0]), reverse = True)
+        
+        for idx,play in sorted_song[:2] :
+            ans.append(idx)
+            
     return ans
         
