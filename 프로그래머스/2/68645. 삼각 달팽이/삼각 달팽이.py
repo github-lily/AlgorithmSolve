3671210279792ed36512b591822a7073233bf70b
이현희 <jhc03032@naver.com>
2026-07-07 19:17:33 +0900

    [level 2] Title: 삼각 달팽이, Time: 125.48 ms, Memory: 45.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.py
@@ -0,0 +1,44 @@
+def solution(n):
+    # 종료 개수 구하기
+    ends = [0] * (n+1)
+    ends[1] = 1
+    for x in range(2,n+1) :
+        ends[x] = ends[x-1] + x
+    
+    end = ends[n]
+    
+    
+    # 기본값 설정
+    arr = [[0]*n for _ in range(n)]
+    di,dj = [1,0,-1],[0,1,-1]       # 하, 우, 좌상대각선 3방향
+    
+    num = 1
+    d = 0
+    ci,cj = 0,0
+    
+    arr[ci][cj] = num
+    num += 1
+
+    
+    # 배열 순회
+    while num <= end :
+        ni, nj = ci + di[d], cj + dj[d]
+        
+        if 0 <= ni < n and 0<= nj < n and arr[ni][nj] == 0 :
+            arr[ni][nj] = num
+            num += 1
+            ci,cj = ni,nj
+        else :
+            d = (d + 1) % 3
+    
+    
+    
+    # 답
+    ans = []
+    for i in range(n) :
+        for j in range(n) :
+            if arr[i][j] != 0 :
+                ans.append(arr[i][j])
+    
+    return ans
+    
