88d266416e3e1b2834acf2477ca6cddff6fe8f76
이현희 <jhc03032@naver.com>
2026-07-12 21:56:59 +0900

    [level 3] Title: 네트워크, Time: 0.40 ms, Memory: 11.7 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,40 +1,34 @@
 from collections import deque
 
 def solution(n, computers):
-    cnt = 0
+    v = [0] * n
     graph = [[] for _ in range(n)]
+    ans = 0
     
-    # 간선 정보 저장
     for i in range(n) :
         for j in range(n) :
-            # 양방향 연결
-            if i != j and computers[i][j] == 1 :
+            if i == j :
+                continue
+            if computers[i][j] == 1 :
                 graph[i].append(j)
-    
-    
+                
+                
     def bfs(start) :
-        nonlocal visit
         q = deque([start])
-        visit[start] = True
-        
+
         while q :
             cur = q.popleft()
-            
             for nxt in graph[cur] :
-                if not visit[nxt] :
-                    visit[nxt] = True
+                if v[nxt] == 0 :
                     q.append(nxt)
+                    v[nxt] = 1
+                    
+    
+    for idx in range(n) :
+        if v[idx] == 1 :
+            continue
+        bfs(idx)
+        ans += 1
+    
+    return ans
         
-        
-    visit = [False] * n
-    
-
-    for i in range(n) :
-        if not visit[i] :
-            cnt += 1
-            bfs(i)
-    
-    return cnt
-    
-    
-    
