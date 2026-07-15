d099ccc58e1c6d45374328fb53cec228f6053565
이현희 <jhc03032@naver.com>
2026-07-15 21:55:20 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.26 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -1,33 +1,36 @@
 import heapq as hq
 
+# 문맥교환에 걸리는 시간 X
 def solution(jobs):
-    q = []
-    i = 0
-    now = 0
-    return_time = 0 
+    wait_q = []
     n = len(jobs)
-    
-    jobs.sort(key = lambda x :x[0])     # 요청시간순으로 정렬
-    
+    times = [0] * n     # 반환시간 저장
+    time = 0
+    idx = 0
 
-    while i < n or q :
-        while i < n and jobs[i][0] <= now :     # 현재시간보다 요청시간이 빠른경우 q에 넣기
-            # 작업의 소요시간이 짧은 것, 작업의 요청 시각이 빠른 것, 작업의 번호가 작은 것 순으로 우선순위
-            # 순서대로 대기 큐에 넣기
-            hq.heappush(q,(jobs[i][1],jobs[i][0]))
-            i += 1
-                        
-            
-        # 우선순위가 높은거 먼저 꺼내기
-        if q :
-            task = hq.heappop(q)
-            now += task[0]                      # 작업 실행
-            return_time += (now - task[1])      # 반환시간 더하기
-            
+    # jobs 요청 시각 순 정렬
+    jobs.sort(key = lambda x : x[0])
+    
+    while wait_q or idx < n :
+        # 요청 시각 되면 대기 큐 삽입
+        while idx < n and time >= jobs[idx][0] :
+            # 소요시간, 요청시각, 작업번호 순 우선순위
+            hq.heappush(wait_q, (jobs[idx][1], jobs[idx][0], idx))
+            idx += 1
+        
+        # 우선순위에 맞게 꺼내기
+        if wait_q :
+            LT, RT, ID = hq.heappop(wait_q)
+            time += LT              # 실행
+            times[ID] = time - RT   # 반환시간 = 현재시간(완료시각) - 요청시각
         else :
-            now = jobs[i][0]                   # 다음 작업 시간으로 점프
-
-    # 개수만큼 나눠서 return
-    ans = return_time // len(jobs)
+            time += 1               # 꺼낼거 없으면 시간 추가
+            
+    
+    ans = sum(times) // n
     return ans
+
+        
+    
+    
     
