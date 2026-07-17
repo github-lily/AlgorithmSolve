6c8ac9f3f9333b08883bd454a6505486eb886dd5
이현희 <jhc03032@naver.com>
2026-07-17 10:28:53 +0900

    [level 2] Title: H-Index, Time: 0.09 ms, Memory: 11.2 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,5 +1,4 @@
 def solution(citations):
-    h = 0
     cnt = 0
     citations.sort(reverse = True)
     
@@ -7,6 +6,8 @@ def solution(citations):
         cnt += 1
         if c >= cnt :
             continue
+        
+        # 현재 개수보다 인용 횟수가 적으면 중단
         else :
             return cnt-1
         
