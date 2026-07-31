c342fce036fd2449872a44df8b535632e6da4322
이현희 <jhc03032@naver.com>
2026-07-31 13:39:54 +0900

    [level 2] Title: 큰 수 만들기, Time: 46.91 ms, Memory: 14.5 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
@@ -1,18 +1,17 @@
-def solution(numbers, k):
+# 순서 고정한 채로 숫자만 제거
+# 뒤의 수가 더 크면 삭제?
+def solution(number, k):
+    n = len(number)
     stack = []
-    for num in numbers :
-        while stack and k > 0 and num > stack[-1] :
-            stack.pop()
-            k -= 1
-        stack.append(num)
 
     
+    for num in number :
+        while stack and stack[-1] < num and k > 0 :
+            stack.pop()     # 작은 값 제거
+            k -= 1          # 제거 개수 차감
+        stack.append(num)   # 작은 값 모두 제거 후 값 추가
+
     if k :
-        ans = "".join(stack[:-k])
-    else :
-        ans = "".join(stack)
-    
-    return ans
-
-
-    
+        return "".join(stack[:-k])
+    return "".join(stack)
+        
