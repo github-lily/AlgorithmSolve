f650684df9b4d0fb884cdc4a79e92ccd9ba9bb90
이현희 <jhc03032@naver.com>
2026-08-13 23:53:40 +0900

    [level 2] Title: 광물 캐기, Time: 0.03 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/2/172927. 광물 캐기/광물 캐기.py
+++ 프로그래머스/2/172927. 광물 캐기/광물 캐기.py
@@ -1,83 +1,52 @@
 def solution(picks, minerals):
-    # 캘 수 있는 광석 수
-    can_cnt = sum(picks) * 5
+    # 캘 수 있는 광석 개수만큼만 계산
+    minerals = minerals[:sum(picks)*5]
+    N = len(minerals)
+    
+    groups = []
+    
+    # 5개씩 그룹으로 묶어 난이도 계산
+    for i in range(0,N,5) :
+        group = minerals[i:i+5]
+        
+        dia = group.count("diamond")
+        iron = group.count("iron")
+        stone = group.count("stone")
+        
+        difficulty = dia * 25 + iron * 5 + stone
+        
+        groups.append((difficulty, dia, iron, stone))
+    
+    # 난이도 내림차순 정렬
+    groups.sort(reverse = True)
+    
+    # 곡괭이
+    pick = 0
     ans = 0
     
-    # 캘 수 있는 광석 만큼만 계산
-    if len(minerals) > can_cnt :
-        minerals = minerals[:can_cnt]
+    # 난이도 높은 순으로 곡괭이 배정
+    for dif, dia, iron, stone in groups :
     
-    N = len(minerals)
-    # 5개 묶음별 피로도
-    gazes = []
-    turn = -1
-    for i in range(0, N, 5) :
-        turn += 1
-        gaze = 0
-        for j in range(5) :
-            if i+j < N:
-                mineral = minerals[i+j]
-                
-                if mineral == 'diamond' :
-                    gaze += 25
-                elif mineral == 'iron' :
-                    gaze += 5
-                else :
-                    gaze += 1
-        
+        while pick < 3 and picks[pick] == 0 :
+            pick += 1
 
-        gazes.append((gaze,turn))
-    print(gazes)
-        
-    # 묶음 난이도가 높은 순으로 정렬
-    gazes.sort(reverse = True)
-    
-
-    # 곡괭이 별 피로도 계산 
-    def cal(gaze, turn) :
-        nonlocal picks
-        
-        total = 0
-        
-        for i in range(3) :
-            if not picks[i] :
-                continue
-                
-            picks[i] -= 1
+        if pick == 3 :      # 곡괭이 없음
+            break
             
-            if i == 0 : # 다이아는 개수만큼만 계산
-                start = turn*5
-                end = min(start+5, N)
-                total = end - start
-                
-            # 철    
-            elif i == 1 :       
-                for idx in range(turn*5, turn*5+5) :
-                    if idx > N-1 :
-                        break
-                    if minerals[idx] == 'diamond' :
-                        total += 5
-                    else :
-                        total += 1
-                        
-            # 돌 = 미리 계산한 값            
-            else :  
-                total = gaze
-            
-                    
-            return total
-                
-
-    # 곡괭이 배정
-    for g,t in gazes :
-        temp = cal(g,t)
-        ans += temp
-    
+        picks[pick] -= 1
+        
+        if pick == 0 :
+            ans += dia + iron + stone
+        
+        elif pick == 1 :
+            ans += dia * 5 + iron + stone
+        
+        else :
+            ans += dif
     
     return ans
         
         
-                
-                
-    
+
+        
     
