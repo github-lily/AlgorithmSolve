b03c1406220d41e11641b62d3beb2b18f8c016ba
이현희 <jhc03032@naver.com>
2026-08-21 23:53:21 +0900

    [level 2] Title: 귤 고르기, Time: 18.26 ms, Memory: 23.6 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
@@ -1,25 +1,10 @@
-def solution(k, gyuls):
+from collections import Counter
+
+def solution(k, tangerine):
+    cnt = sorted(Counter(tangerine).values(), reverse=True)
     
-    # 귤 개수 저장
-    cnt_dict = {}
-    
-    for g in gyuls :
-        if g not in cnt_dict :
-            cnt_dict[g] = 0
-        cnt_dict[g] += 1
-    
-    # 귤 고르기
-    vals = list(cnt_dict.values())      # 개수만 필요
-    vals.sort(reverse = True)
-    ans = 0
-    
-    for v in vals :
-        k -= v
-        ans += 1
-        if k <= 0 :
-            return ans
-    
-    return ans
-        
-    
-    
+    total = 0
+    for i, n in enumerate(cnt):
+        total += n
+        if total >= k:
+            return i + 1
