8f3627f6606763deffe06766f03a89dcb41d2ca2
이현희 <jhc03032@naver.com>
2026-08-23 00:13:14 +0900

    [level 2] Title: 피로도, Time: 20.06 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,25 +1,27 @@
-def solution(k, duns):
-    n = len(duns)
-    v = [False] * n
+def solution(k, dungeons):
     mx = 0
+    n = len(dungeons)
+    v = [0] * n
     
-    def dfs(health, cnt) :
+    def dfs(stamina, cnt) :
         nonlocal mx
         
-        mx = max(cnt,mx)
+        if mx < cnt :
+            mx = cnt
         
-        if health <= 0 :
+        if stamina <= 0 :
             return
         
         for i in range(n) :
-            if v[i] == 0 and duns[i][0] <= health :
-                v[i] = True
-                dfs(health - duns[i][1], cnt + 1)
-                v[i] = False
-        
-    
-    dfs(k,0)
+            need, burn = dungeons[i]
+            if stamina >= need and v[i] == 0 :
+                v[i] = 1
+                dfs(stamina - burn, cnt + 1)
+                v[i] = 0
+                
+    dfs(k, 0)
     
     return mx
-
+            
+        
         
