4f78330471cbde3ab704420cc4a62541c1061d8b
이현희 <jhc03032@naver.com>
2026-08-25 23:50:25 +0900

    [level 2] Title: 구명보트, Time: 7.56 ms, Memory: 11.7 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -1,24 +1,15 @@
-# 구명보트 한 번에 최대 2명 탑승 가능
 def solution(people, limit):
     people.sort()
-    
-    left = 0                # 가벼운 사람
-    right = len(people)-1     # 무거운 사람
-    boat = 0
-    
-    while left <= right :
-        light = people[left]
-        heavy = people[right]
-        
-        if light + heavy <= limit :
+
+    left = 0
+    right = len(people) - 1
+    answer = 0
+
+    while left <= right:
+        if people[left] + people[right] <= limit:
             left += 1
 
         right -= 1
-        boat += 1
-    
-    return boat
-    
-        
-        
-    
-    
+        answer += 1
+
+    return answer
