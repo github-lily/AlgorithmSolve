497132aaa19878cbf0f8c00bbad3408911d89f1b
이현희 <jhc03032@naver.com>
2026-08-31 23:52:31 +0900

    [level 2] Title: 더 맵게, Time: 1516.57 ms, Memory: 71.8 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -1,31 +1,26 @@
-import heapq
+import heapq as hq
 
 def solution(scovilles, K):
     cnt = 0
     q = []
-    for scoville in scovilles :
-        heapq.heappush(q,scoville)
     
-    while q :        
-        # 종료 조건 : q의 최소값이 K 이상 
-        if q[0] >= K :
+    for s in scovilles :
+        hq.heappush(q,s)
+    
+    while q :            
+        cur = hq.heappop(q)
+        if cur >= K :
             return cnt
         
-        if len(q) == 1 :
-            if q[0] < K :
-                return -1
+        if not q :
+            return -1
         
-        # 안되면 섞기
-        x1 = heapq.heappop(q)
-        x2 = heapq.heappop(q)
-        
-        new = x1 + (x2 * 2)
+        nxt = hq.heappop(q)
+        hq.heappush(q,(cur + nxt*2))
         cnt += 1
-        
-        heapq.heappush(q,new)
-        
+    
     return -1
-            
-            
         
-        
+    
+    
+    
