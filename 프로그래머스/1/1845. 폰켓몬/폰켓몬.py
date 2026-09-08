8f1e592674cda0e514cdb600cc7e8720db742339
이현희 <jhc03032@naver.com>
2026-09-08 23:49:32 +0900

    [level 1] Title: 폰켓몬, Time: 0.56 ms, Memory: 12.5 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,7 +1,6 @@
 def solution(nums):
-    N = len(nums)
-    nums = set(nums)
-    if len(nums) >= N//2 :
-        return N//2
-    else :
-        return len(nums)
+    can_get = len(nums) // 2        # 항상 짝수
+    nums_set_lenn = len(set(nums))
+    
+    return min(can_get, nums_set_lenn)
+    
