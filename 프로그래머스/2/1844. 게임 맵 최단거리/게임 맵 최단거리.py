ba09ad7a189b7250fa6708457348adc13a6ab4fd
이현희 <jhc03032@naver.com>
2026-09-09 22:43:38 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.79 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -3,22 +3,32 @@ from collections import deque
 def solution(maps):
     n = len(maps)
     m = len(maps[0])
-    di,dj = [1,0,-1,0],[0,1,0,-1]
     
-    visit = [[-1] * m for _ in range(n)]
-    q = deque([(0,0)])
-    visit[0][0] = 1
+    si, sj = 0,0
+    q = deque([(si,sj)])
+    
+    v = [[0] * m for _ in range(n)]
+    v[0][0] = 1
+    
+    di,dj = [0,1,0,-1],[1,0,-1,0]
+    
     
     while q :
         ci,cj = q.popleft()
         
         if ci == n-1 and cj == m-1 :
-            return visit[ci][cj]
+            return v[ci][cj]
         
         for d in range(4) :
-            ni,nj = ci + di[d], cj+dj[d]
-            if 0<= ni < n and 0<= nj < m and maps[ni][nj] == 1 and visit[ni][nj] == -1 :
-                visit[ni][nj] = visit[ci][cj] + 1
-                q.append((ni,nj))
+            ni,nj = ci + di[d], cj + dj[d]
+            if 0 <= ni < n and 0 <= nj < m :
+                if maps[ni][nj] == 1 and v[ni][nj] == 0 :
+                    v[ni][nj] = v[ci][cj] + 1
+                    q.append((ni,nj))
+        
+        
+    return -1 
+        
+
     
-    return -1
+    
