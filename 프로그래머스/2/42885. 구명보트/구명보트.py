99fe197678d9f96fa691abe999253a13337d0c96
이현희 <jhc03032@naver.com>
2026-09-12 01:15:01 +0900

    [level 2] Title: 구명보트, Time: 8.02 ms, Memory: 11.8 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -1,15 +1,27 @@
 def solution(people, limit):
-    people.sort()
-
+    people.sort(reverse = True)
+    lenn = len(people)
+    
     left = 0
-    right = len(people) - 1
-    answer = 0
-
-    while left <= right:
-        if people[left] + people[right] <= limit:
-            left += 1
-
-        right -= 1
-        answer += 1
-
-    return answer
+    right= lenn-1
+    cnt = 0
+    
+    while left <= right :
+        # 마지막 사람
+        if left == right :
+            cnt += 1
+            break
+            
+        # 2명 탑승
+        if people[left] + people[right] <= limit :
+            right -= 1
+        
+        # 왼쪽 한 명은 무조건 탑승
+        left += 1
+        cnt += 1
+    
+    return cnt
+            
+                    
+                
+    
