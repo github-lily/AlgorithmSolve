13aebfb253351f6214b356196aed564915c888a5
이현희 <jhc03032@naver.com>
2026-09-22 21:52:22 +0900

    [level 3] Title: 네트워크, Time: 0.21 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,34 +1,34 @@
 from collections import deque
 
 def solution(n, computers):
-    v = [0] * n
-    graph = [[] for _ in range(n)]
     ans = 0
+    start = 0
     
+    graph = [[] for _ in range(n)]
+    v = [0] * n
+    
+    q = deque()
+
     for i in range(n) :
-        for j in range(n) :
+        for j in range(i+1,n) :
             if i == j :
                 continue
             if computers[i][j] == 1 :
                 graph[i].append(j)
-                
-                
-    def bfs(start) :
-        q = deque([start])
-
-        while q :
-            cur = q.popleft()
-            for nxt in graph[cur] :
-                if v[nxt] == 0 :
-                    q.append(nxt)
-                    v[nxt] = 1
-                    
+                graph[j].append(i)
     
-    for idx in range(n) :
-        if v[idx] == 1 :
-            continue
-        bfs(idx)
-        ans += 1
+    for i in range(n) :
+        if v[i] == 0 :
+            q.append(i)
+            ans += 1
+            while q :
+                cur = q.popleft()
+                
+                for nxt in graph[cur] :
+                    if v[nxt] == 0 :
+                        v[nxt] = 1
+                        q.append(nxt)
     
     return ans
-        
+
+    
