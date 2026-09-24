8417fc4094f10ff0fede5059cd65b1c6bf1642be
이현희 <jhc03032@naver.com>
2026-09-24 19:54:37 +0900

    [level 2] Title: 더 맵게, Time: 1651.36 ms, Memory: 52.4 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -1,26 +1,20 @@
-import heapq as hq
+import heapq
 
-def solution(scovilles, K):
-    cnt = 0
-    q = []
-    
-    for s in scovilles :
-        hq.heappush(q,s)
-    
-    while q :            
-        cur = hq.heappop(q)
-        if cur >= K :
-            return cnt
-        
-        if not q :
-            return -1
-        
-        nxt = hq.heappop(q)
-        hq.heappush(q,(cur + nxt*2))
-        cnt += 1
-    
-    return -1
-        
-    
-    
-    
+def solution(scoville, K):
+    heapq.heapify(scoville)
+    answer = 0
+
+    while len(scoville) >= 2 and scoville[0] < K:
+        # 가장 맵지 않은 두 음식 추출
+        first = heapq.heappop(scoville)
+        second = heapq.heappop(scoville)
+
+        # 새 스코빌 지수 추가
+        heapq.heappush(scoville, first + second * 2)
+        answer += 1
+
+    # K 이상으로 만들 수 없는 경우
+    if scoville[0] < K:
+        return -1
+
+    return answer
